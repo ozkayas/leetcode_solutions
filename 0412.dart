d5e2e53a4bfe73e3f5ffe6fe2b07dbf215c36df3
Suat Özkaya <suatozkaya@gmail.com>
2022-12-29 22:26:03 +0300

    Add solution

---
+++ 0412.dart
@@ -0,0 +1,30 @@
+import 'dart:collection';
+import 'dart:math';
+
+main() {
+  int n = 15;
+  var result = Solution().fizzBuzz(n);
+  return 0;
+}
+
+class Solution {
+  Map<int, String> hashTable = <int, String>{
+    3: "Fizz",
+    5: "Buzz",
+  };
+
+  List<String> fizzBuzz(int n) {
+    List<String> result = List<String>.filled(n, "");
+    for (int i = 1; i <= n; i++) {
+      String item = "";
+      hashTable.forEach((key, value) {
+        if (i % key == 0) {
+          item += value;
+        }
+      });
+      if (item.isEmpty) item = i.toString();
+      result[i - 1] = item;
+    }
+    return result;
+  }
+}
