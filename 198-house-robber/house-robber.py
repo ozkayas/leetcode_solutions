d724d820607c12f4521d3a38918d692bc73c9b38
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-01-21 17:13:21 +0300

    Time: 39 ms (54.78%) | Memory: 16.6 MB (56.96%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,25 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         memo = {}
-        N = len(nums)-1
-        if N == 0: return nums[0]
-        if N == 1: return max(nums[0], nums[1])
 
         def fn(i):
-            if i in memo:
-                return memo[i]
-            
-
-            if i == N:
-                return nums[N]
-            if i == N-1:
-                return max(nums[N-1], nums[N])
-            
-            memo[i] = max(  nums[i]+fn(i+2) , fn(i+1) )
+            if i < 0: return 0
+            if i not in memo:
+                memo[i] =  max(nums[i]+fn(i-2), fn(i-1))
             return memo[i]
+            
 
 
-        return fn(0)
-
-
-        
+        return fn(len(nums)-1)     
