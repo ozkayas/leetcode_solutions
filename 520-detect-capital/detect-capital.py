1cc83c8ad597afbb954f926d0e8fde7a231c3bdb
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-02-06 23:31:18 +0300

    Time: 34 ms (74.70%) | Memory: 16.6 MB (66.07%) - LeetSync

--- 520-detect-capital/detect-capital.py
+++ 520-detect-capital/detect-capital.py
@@ -2,25 +2,19 @@ class Solution:
     def detectCapitalUse(self, word: str) -> bool:
         # A : 65 Z: 90 a: 97 z: 122
         capitals = 0
-        nonCapitals = 0
         isFirstCapital = 65<=ord(word[0])<=90
 
         for i in range(len(word)):
             if 65<=ord(word[i])<=90:
                 capitals += 1
-            else:
-                nonCapitals += 1
         
-        if isFirstCapital:
-            if nonCapitals == 0:
-                return True
-            if capitals == 1:
-                return True
-            return False
-        else:
-            if capitals > 0:
-                return False
+        if capitals == len(word):
+            return True
+        if isFirstCapital and capitals == 1:
+            return True
+        if capitals ==  0:
+                return True 
         
-        return True
+        return False 
 
         
