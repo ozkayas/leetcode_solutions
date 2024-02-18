b29bb525e647aaf59e3106530380ebba01a0dbdc
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-02-18 22:59:07 +0300

    Time: 358 ms (80.11%) | Memory: 149 MB (73.67%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.dart
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.dart
@@ -1,17 +1,46 @@
 class Solution {
   int removeDuplicates(List<int> nums) {
 
-      final s = nums.toSet();
-    //   print(s);
-      final l = s.toList();
-      l.sort();
-    //   print(l);
+      int r = 0, w = 0;
 
-    for (int i = 0; i < l.length; i++){
-        nums[i] = l[i];
-    }
+      while(r < nums.length){
+          if(nums[r] > nums[w]){
+              w++;
+              nums[w] = nums[r];
+          }
+
+          r ++;
+      }
+
+      return w + 1;
+
+
+ // r ana while loopda sonra kadar gidecek
+ // r > w ->  w++ , yazma islemi
+
+//                       r
+//  [0,1,2,3,4,2,2,3,3,4]
+//           w
+
+// return w + 1;
+
+
+
+
+
+
+
+    //   final s = nums.toSet();
+    // //   print(s);
+    //   final l = s.toList();
+    //   l.sort();
+    // //   print(l);
+
+    // for (int i = 0; i < l.length; i++){
+    //     nums[i] = l[i];
+    // }
     
     
-    return l.length;
+    // return l.length;
   }
 }
