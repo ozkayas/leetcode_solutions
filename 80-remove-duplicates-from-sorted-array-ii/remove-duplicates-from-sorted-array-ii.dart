b65275dc7b45b5ba5b46d660a5d5a155724f5025
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-02-19 22:38:26 +0300

    Time: 388 ms (22.79%) | Memory: 151.8 MB (10.13%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.dart
@@ -0,0 +1,31 @@
+class Solution {
+  int removeDuplicates(List<int> nums) {
+        int r = 0, w = 0;
+        var map = {};
+
+        while(r < nums.length){
+            map[nums[r]] = (map[nums[r]] ?? 0) + 1;
+
+            if(nums[r] > nums[w]){
+                w++;
+                nums[w] = nums[r];
+            }
+
+            if(nums[r] == nums[w] && map[nums[r]] == 2){
+                w++;
+                nums[w] = nums[r];   
+            }
+
+            r ++;
+        }
+
+    return w + 1;
+  }
+}
+
+// herhangi bir sayiya 2. kez denk gelirsek w++ ve ustune yaz
+// ayni sayiya 2. ken denk geldigimi bilmek iicn bir map tutacagiz.
+
+//                    r
+// [0,0,1,1,2,3,3,3,3]
+//              w
