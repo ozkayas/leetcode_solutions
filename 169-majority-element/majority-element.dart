a3c6af8c236180a4c14947b64deaf6b4ef667e6b
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-02-21 22:41:22 +0300

    Time: 371 ms (38.51%) | Memory: 153.6 MB (43.92%) - LeetSync

--- 169-majority-element/majority-element.dart
+++ 169-majority-element/majority-element.dart
@@ -1,18 +1,50 @@
 class Solution {
   int majorityElement(List<int> nums) {
-      var map = {};
-      int res = 0;
-
+      int k = 0, s =0;
       for(int n in nums){
-          map[n] = 1 + (map[n] ?? 0);
+          if(s == 0){
+              s++;
+              k = n;
+          }
+          else{
+              if(k == n){
+                  s++;
+              }else{
+                  s--;
+              }
 
-            if(map[n] > nums.length/2){
-                res = n;
-                break;
-            }
-
+          }
       }
+
+    return k;
+
+
+
+
+    // kazanan= 2
+    // sayac= 3
+
+
+    // [,,,2,2,2]  == > 11 eleman 6 adet 2  
+    //                      |
+
+/// sayac 0 ise , sayac ++ , kazanan = current
+/// else kaann = cur, sayaca ++
+/// cur != kazanan, sayaca --
+
+    //   var map = {};
+    //   int res = 0;
+
+    //   for(int n in nums){
+    //       map[n] = 1 + (map[n] ?? 0);
+
+    //         if(map[n] > nums.length/2){
+    //             res = n;
+    //             break;
+    //         }
+
+    //   }
     
-    return res;
+    // return res;
   }
 }
