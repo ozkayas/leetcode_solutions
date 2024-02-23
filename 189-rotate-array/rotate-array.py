1a081f204bfaac75a64aa103e2c2849221923303
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-02-23 23:39:51 +0300

    Time: 162 ms (55.22%) | Memory: 27.9 MB (95.49%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,16 +1,43 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
+        k = k % len(nums)
+        l,r = 0 , len(nums)-1
 
-        res = []
-        i = 0
-        while i < len(nums):
-            ii = i + len(nums) - k  #i=0 ii=4
-            ii = ii % len(nums)
-            # print(ii)
-            res.append(nums[ii])
-            i += 1
+        while l<r:
+            nums[l],nums[r] = nums[r],nums[l]
+            l += 1
+            r -= 1
         
-        for i in range(len(nums)):
-            nums[i] = res[i]
-            
+        l, r = 0, k-1
+        while l<r:
+            nums[l],nums[r] = nums[r],nums[l]
+            l += 1
+            r -= 1
+
+        l, r = k, len(nums)-1
+        while l<r:
+            nums[l],nums[r] = nums[r],nums[l]
+            l += 1
+            r -= 1
+
+        print(nums)
+
+
+
+
+
+
+### Solutiion 1
+        # res = []
+        # i = 0
+        # while i < len(nums):
+        #     ii = i + len(nums) - k  #i=0 ii=4
+        #     ii = ii % len(nums)
+        #     # print(ii)
+        #     res.append(nums[ii])
+        #     i += 1
+        
+        # for i in range(len(nums)):
+        #     nums[i] = res[i]
+
             
