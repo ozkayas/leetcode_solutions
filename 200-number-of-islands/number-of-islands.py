ef307787aea50c64459b216843f102224fe0a543
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-02-25 00:08:47 +0300

    Time: 228 ms (86.99%) | Memory: 18.9 MB (75.59%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,34 +1,22 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-        # visited = set()
-        count = 0
+        R, C = len(grid), len(grid[0])
+        counter = 0
 
         def dfs(r,c):
-
-            # visited.add((r,c))
             grid[r][c] = "0"
-            #check N
-            if r -1 >= 0 and grid[r-1][c] == "1":
-                dfs(r-1,c)
-            #check S
-            if r +1 <= rows-1 and grid[r+1][c] == "1":
-                dfs(r+1,c)
-            #check E
-            if c+1 <= cols-1 and grid[r][c+1] == "1" :
-                dfs(r,c+1)
-            #check w
-            if c -1 >= 0 and grid[r][c-1] == "1":
-                dfs(r,c-1)
-    
+            
+            for rr,cc in (r+1,c),(r-1,c),(r,c+1),(r,c-1):
+                if 0 <= rr < R and 0 <= cc < C and grid[rr][cc] == "1":
+                    dfs(rr,cc)
 
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == "1":
-                    count+= 1
-                    grid[r][c] = "0"
-                    dfs(r,c)
+
+
         
+        for r in range(R):
+            for c in range(C):
+                if grid[r][c] == "1":
+                    counter += 1
+                    dfs(r,c)
 
-        return count
+        return counter
