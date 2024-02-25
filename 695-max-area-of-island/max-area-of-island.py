f20d9d5fb4e6241bf5b25cf1c2e26c30b4d016fd
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-02-25 21:48:15 +0300

    Time: 116 ms (45.86%) | Memory: 17.9 MB (47.61%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,27 +1,22 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-        res = [0]
-        counter = [0]
-        
+        R,C = len(grid),len(grid[0])
+        res, counter = [0], [0]
+
         def dfs(r,c):
-            # print(r,c)
-            grid[r][c] = 0
+            grid[r][c] = 0 #sink the grid
             counter[0] += 1
-            res[0] = max(res[0],counter[0])
-            # print('res',res[0])
-            for rr,cc in (r-1,c),(r+1,c),(r,c-1),(r,c+1):
-                if 0 <= rr < rows and 0 <= cc < cols and grid[rr][cc] == 1:
-                    # print("i", i)
+            res[0] = max(res[0], counter[0])
+
+            for rr,cc in (r+1,c),(r-1,c),(r,c-1),(r,c+1):
+                if -1 < rr < R and -1 < cc < C and grid[rr][cc] == 1:
+                
                     dfs(rr,cc)
 
-        
-        for r in range(rows):
-            for c in range(cols):
+        for r in range(R):
+            for c in range(C):
                 if grid[r][c] == 1:
                     counter[0] = 0
                     dfs(r,c)
-
-
+        
         return res[0]
