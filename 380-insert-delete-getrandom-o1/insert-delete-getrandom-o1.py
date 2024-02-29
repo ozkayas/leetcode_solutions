0af107f0054aedde71273f54d2a5aeec2b634b94
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-03-01 00:02:59 +0300

    Time: 245 ms (92.90%) | Memory: 63.7 MB (87.74%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -2,24 +2,36 @@ class RandomizedSet:
 
     def __init__(self):
         self.map = dict()
+        self.arr = []
         
     def insert(self, val: int) -> bool:
         if val not in self.map:
-            self.map[val] = True
+            self.arr.append(val)
+            iOfVal = len(self.arr)-1
+            self.map[val] = iOfVal
+            # print("insert", self.arr, self.map)
             return True
         else:
             return False
 
     def remove(self, val: int) -> bool:
         if val in self.map:
-            del self.map[val]
+            iOfVal = self.map[val]
+            lastItem = self.arr[-1]
+            self.map[lastItem] = iOfVal
+            self.arr[iOfVal] = lastItem
+            del self.map[val]          
+            
+            self.arr.pop()
+            # print("removed", self.arr, self.map)
+
             return True
         else:
             return False
         
 
     def getRandom(self) -> int:
-        return random.choice(list(self.map))
+        return random.choice(self.arr)
         
 
 
@@ -28,3 +40,11 @@ class RandomizedSet:
 # param_1 = obj.insert(val)
 # param_2 = obj.remove(val)
 # param_3 = obj.getRandom()
+
+'''
+ 0 1 2 3 
+[3,1,10,5,]
+
+
+{3:0, 1:1, 4:2, 10:2}  // val = index
+'''
