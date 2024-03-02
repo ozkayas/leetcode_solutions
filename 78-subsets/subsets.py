4bd9e2e447b1ea41c7b7062a0deb8e5b8e302f93
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-03-02 20:18:12 +0300

    Time: 41 ms (31.63%) | Memory: 16.7 MB (51.88%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,63 +1,12 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        if not nums: return []
- 
-        def rec(digit) -> List[List[int]]:
-            if digit == len(nums) -1:
-                return [[nums[digit]],[]]
+        res = [[]]
 
-            nextNodeRes = rec(digit+1)
-            res = []
-            for n in nextNodeRes: # sample [[3], []]
-      
-                res.append(n)
-       
-                a = n.copy()
-                a.append(nums[digit])
-    
-                res.append(a)
-     
-
-            return res
-
-        return rec(0)
+        for n in nums:
+            for i in range(len(res)):
+                newSubset = res[i] + [n]
+                res.append(newSubset)
+                
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
- 
-'''       
-        n = len(nums)
-        res = []
-
-        for i in range(2**n):
-            subItem = []
-
-            for b in range(n):
-                if i & 1 << b : subItem.append(nums[b])
-
-            res.append(subItem)
-        
         return res
-
-        # Watch this for explanation
-        # https://www.youtube.com/watch?v=b7AYbpM5YrE
-
-'''
