0dda8939079eb0b85082152a855e16c8b6b3f1f5
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-03-17 16:23:55 +0300

    Time: 73 ms (71.36%) | Memory: 20 MB (39.60%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,21 +1,23 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        if not intervals: return [newInterval]
-        ans = []
+        res = [] 
+        intervalAdded = False
 
-        for i, interval in enumerate(intervals):
-            #if no intersection and before new -> just append to result
+        for i,interval in enumerate(intervals):
+            # no intersection just continue
             if interval[1] < newInterval[0]:
-                ans.append(interval)
-            #if no intersection and after new -> append new and append the rest
-            elif interval[0] > newInterval[1]:
-                ans.append(newInterval)
-                ans += intervals[i:]
-                return ans
-            # there is an intersection so just merge interval to newInterval
+                res.append(interval)
+            elif newInterval[1] < interval[0]:
+                res.append(newInterval)
+                res += intervals[i:]
+                return res
+                
             else:
-                newInterval[0] = min(newInterval[0], interval[0])
-                newInterval[1] = max(newInterval[1], interval[1])
+                newInterval[0] = min(interval[0], newInterval[0])
+                newInterval[1] = max(interval[1], newInterval[1])
 
-        ans.append(newInterval)
-        return ans
+        res.append(newInterval)
+        return res
+
+
+
