294cc7f768c0cd3fc50db0c4449e46cea5f3c4b2
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-03-22 19:43:34 +0300

    Time: 36 ms (63.37%) | Memory: 16.7 MB (30.86%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,12 +1,30 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = [[]]
 
-        for n in nums:
-            for i in range(len(res)):
-                newSubset = res[i] + [n]
-                res.append(newSubset)
-                
+        ans = []
+
+        def bt(i, subset):
+            if i == len(nums):
+                ans.append(subset.copy())
+                return
+            
+            subset.append(nums[i])
+            bt(i + 1, subset)
+            subset.pop()
+            bt(i + 1, subset)
+
+        
+        bt(0, [])
+
+        return ans
 
 
-        return res
+
+
+
+
+
+
+
+    
+        
