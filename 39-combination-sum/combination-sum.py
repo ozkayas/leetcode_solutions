6230fb0da87d836adb9afc32b4490167fe014da2
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-03-24 22:10:21 +0300

    Time: 32 ms (99.78%) | Memory: 16.6 MB (85.43%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -7,35 +7,15 @@ class Solution:
             if target == 0:
                 ans.append(comb.copy())
                 return
-            
-            if i == len(candidates) or candidates[i] > target:
+            if i >= len(candidates) or candidates[i] > target:
                 return
-
-            comb.append(candidates[i])
-            dfs(i, target - candidates[i], comb)
+            cur = candidates[i]
+            
+            comb.append(cur)
+            dfs(i, target - cur, comb)
             comb.pop()
-            dfs(i+1, target, comb)
+            dfs(i+1, target , comb)
 
-        dfs(0, target, [])
-
+        dfs(0, target, [])        
         return ans
-
-
-
-
-
-
-
-
-'''
-
-[2,3,6,7]
-[2,3,6,7]
-[2,3,6,7]
-[2,3,6,7]
-[2,3,6,7]
-[2,3,6,7]
-[2,3,6,7]
-
-
-'''
+        
