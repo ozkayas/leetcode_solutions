61e2eaf1b2bf06b82354c9c88843fe3800d7e3f8
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-03-24 21:48:13 +0300

    Time: 32 ms (85.04%) | Memory: 16.7 MB (30.79%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,21 +1,20 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        N = len(nums)
+
         ans = []
-        
         def dfs(i, subset):
-            if i == N:
+            if i == len(nums):
                 ans.append(subset.copy())
                 return
             
             subset.append(nums[i])
-            dfs(i+1, subset)
-
+            dfs(i+1,subset)
             subset.pop()
-            dfs(i+1, subset)
+            dfs(i+1,subset)
 
+        
 
+        dfs(0,[])
 
-        dfs(0,[])    
         return ans
         
