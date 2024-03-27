58d1938cbc787bc491593ac74cd9edec00def390
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-03-27 11:58:23 +0300

    Time: 43 ms (18.12%) | Memory: 16.8 MB (30.88%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -3,14 +3,14 @@ class Solution:
         ans = []
 
         def backTrack(i, subset):
-            print("BT called i, subset",i, subset)
+            # print("BT called i, subset",i, subset)
             #bu son index ise, leaf node, subsete kendin ekle ve sonuca ekle ve return
             if i == len(nums)-1:
                 s = subset.copy()
                 ans.append(s.copy())
                 s.append(nums[i])
                 ans.append(s.copy())
-                print("leaf node returning", ans)
+                # print("leaf node returning", ans)
                 return
 
             subset.append(nums[i])
