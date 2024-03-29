51712acdd213981d2fd2ecf9accd3bcd6f330772
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-03-29 23:28:11 +0300

    Time: 1168 ms (41.24%) | Memory: 32.3 MB (6.03%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.py
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.py
@@ -1,21 +1,33 @@
 class Solution:
     def maxSubarrayLength(self, nums: List[int], k: int) -> int:
-        N = len(nums)
-        l,r = 0,0
+        l,r, ans = 0, 0, 0
+        N =  len(nums)
         m = dict()
-        ans = 0
 
         while r < N:
-            rNum = nums[r]
-            m[rNum] = 1 + m.get(nums[r], 0)
-            if m[rNum] <= k:
-                ans = max(ans, (r-l+1))
-            else:
-                while m[rNum] > k:
+            curR = nums[r]
+            m[curR] = 1 + m.get(curR, 0)
+            if m[curR] > k:
+                while l < r and m[curR] > k:
                     m[nums[l]] -= 1
                     l += 1
+            else:
+                ans = max(ans, (r-l+1))
+                # print(r-l+1)
+            r+=1
 
-
-            r += 1 
-        
         return ans
+
+        
+'''          r
+        [1,4,4,3].    k = 1
+         l
+  hMap :
+      1: 1 
+      4: 2
+      3: 
+  
+
+   ans : 1,   
+
+   // r nin bulundugu degerdeki fazlalik azalana kadar l yi saga shift et, while ile ve '''
