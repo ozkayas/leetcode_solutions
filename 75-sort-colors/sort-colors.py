f48a6fbbf36de76796602de14f7d0e3536da931f
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-04-12 02:00:12 +0300

    Time: 40 ms (36.64%) | Memory: 16.6 MB (42.28%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,15 +3,27 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        counter = Counter(nums)
-        # print(counter[0])
+        pivot = 1
+        # In the end, l and r will be boundaries. we will loop while m <= r
+        # m is the reader pointer
+        # 0 0 0 0 1 1 1 2 2 2 2
+        #         l   r
 
-        for i in range(len(nums)):
-            if counter[0] > 0:
-                nums[i] = 0
-                counter[0] -= 1
-            elif counter[1] > 0:
-                nums[i] = 1
-                counter[1] -= 1
+        l, m, r = 0, 0, len(nums)-1
+
+        while m <= r:
+            if nums[m] > pivot:
+                nums[m], nums[r] = nums[r], nums[m]
+                r -= 1
+            elif nums[m] < pivot:
+                nums[m], nums[l] = nums[l], nums[m]
+                l += 1
+                m +=1
             else:
-                nums[i] = 2
+                m += 1
+        
+
+
+        
+
+
