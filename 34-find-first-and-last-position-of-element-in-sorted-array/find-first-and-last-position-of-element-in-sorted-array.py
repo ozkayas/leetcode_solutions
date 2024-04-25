0337e22bba730a82405ecbba60a419a16bc39a94
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-04-25 22:41:20 +0300

    Time: 65 ms (97.06%) | Memory: 17.8 MB (47.30%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,67 +1,39 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if not nums: return [-1, -1]
-        idx = -1
-        res = [-1, -1]
+        N = len(nums)
+        # we need 2 binary search
+        # 2 3 3 4 5 8 8 8 8 9 9 10 
+        #           s     e
+        startIndex, endIndex = -1, -1
+        ## 1 Binary Search for target <= middle
+        ## To find starting of the target
 
-        #First binary search to catch any index of target, or return -1 and finish search
-        l, r = 0, len(nums) - 1
-
+        l, r = 0, N-1
         while l <= r:
             m = l + (r-l)//2
 
-            if nums[m] == target:
-                idx = m
-                break
-            if target < nums[m]:
+            if nums[m] >= target:
+                # We are ok, but continue search the first appearance of the 8, or 9 ...
+                startIndex = m
                 r = m -1
             else:
-                l = l +1
+                l = m +1
+        if startIndex == -1 or nums[startIndex] != target:
+            # BS ended but can not find the target, propably bigger than 8, target
+            return [-1, -1]
 
-        if idx == -1:
-            return res
-        
-        #Second binary search to detect start of the target
-        l, r = 0, idx
-        ans = -1
+        ## 2. Binary Search for middle <= target
+        ## Last of the target
+        l, r = startIndex, N -1 # we can start l from start index
+
         while l <= r:
             m = l + (r-l)//2
 
-            if target <= nums[m]:
-                ans = m
-                r = m - 1
-            else:
-                l = m + 1
-
-        res[0] = ans
-
-        # Third binary search to detect the ending of the target
-        l, r = idx, len(nums)-1
-        ans = -1
-        print(nums[idx:])
-        while l <= r:
-            m = l + (r-l)//2
-
-            if target >= nums[m]:
-                ans = m
+            if nums[m] <= target:
+                # We are ok, but should search to the right
+                endIndex = m
                 l = m + 1
             else:
                 r = m - 1
 
-        res[1] = ans
-
-
-        return res
-
-    
-'''
-#second binary search
- t t t f f f   <- find first false
-[5,7,7,8,8,8]
-       ^
-
-#third binary search
- t t t t f f   <- find last true
-[8,8,8,8,9,10]
-     ^
-'''
+        return [startIndex, endIndex]
