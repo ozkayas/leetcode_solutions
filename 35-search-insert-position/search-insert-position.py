5aff1c69ca89c00db47401d503c1882071d217b0
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-04-25 15:33:21 +0300

    Time: 39 ms (97.69%) | Memory: 17.4 MB (49.48%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,21 +1,26 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        # Searching for x >= target question, searching for the index actually
-        l, r = 0, len(nums)-1
-        idx = -1
 
-        if target > nums[-1]:
-            return len(nums)
+        # searching for >= target 
+        # [1,3,5,6] 
+        #  f t t t
+
+        # edge case if targe > last 
+        if target > nums[-1]: return len(nums)
+
+        l, r  = 0, len(nums)-1
+        ans = -1
 
         while l <= r:
+            
             m = l + (r-l)//2
 
             if target <= nums[m]:
-                idx = m
-                r = m - 1
+                ans = m
+                r = m -1
             else:
-                l = m + 1
+                l = m +1
         
-        return idx
+        return ans
 
         
