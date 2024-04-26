3b40321a9e9086a597c6561e2a91cc3570acce2f
Suat Özkaya <suatozkaya@gmail.com>
2024-04-26 08:25:52 +0300

    Find median

--- 9999-A2Z-OA/Find Median of Subarray Uniqueness.py
+++ 9999-A2Z-OA/Find Median of Subarray Uniqueness.py
@@ -1,10 +1,14 @@
 '''
 In an Amazon coding marathon, the following challenge was given.
 
-The uniqueness of an array of integers is defined as the number of distinct elements present. For example, the uniqueness of [1, 5, 2, 1, 3, 5] is 4, element values 1, 2, 3, and 5. For an array arr of n integers, the uniqueness values of its subarrays is generated and stored in another array, call it subarray_uniqueness for discussion. Find the median of the generated array subarray_uniqueness.
+The uniqueness of an array of integers is defined as the number of distinct elements present. For example, the uniqueness of [1, 5, 2, 1, 3, 5] is 4, element values 1, 2, 3, and 5. 
+For an array arr of n integers, the uniqueness values of its subarrays is generated and stored in another array, call it subarray_uniqueness for discussion. 
+Find the median of the generated array subarray_uniqueness.
 
 Notes:
-The median of a list is defined as the middle value of the list when it is sorted in non-decreasing order. If there are multiple choices for median, the smaller of the two values is taken. For example, the median of [1, 5, 8] is 5, and of [2, 3, 7, 11] is 3.
+The median of a list is defined as the middle value of the list when it is sorted in non-decreasing order. 
+If there are multiple choices for median, the smaller of the two values is taken. 
+For example, the median of [1, 5, 8] is 5, and of [2, 3, 7, 11] is 3.
 A subarray is a contiguous part of the array. For example, [1, 2, 3] is a subarray of [6, 1, 2, 3, 5] but [6, 2] is not.
 Function Description
 
@@ -69,48 +73,72 @@ The subarray_uniqueness array is [1, 2, 2, 1, 2, 1]. After sorting, the arr is [
 
 '''
 ### My Explanation
-  subarray_median = [ ]
-  
 
-[1, 2, 3]  
+[1, 2, 3] 
 
-# Discover subarrays with 2 pointers.
-  at each iteration we can also take the median using the pointers
- ## Ex: 1 2 3 4 5 pointers 
-        l     r      l = 0 and r = 3 , should get  1   
-        l       r    l = 0 and r = 4 , should get  2
-          l     r    l = 1 and r = 4 , should get  2
-        # formula for median is (l + r) // 2
+ 1        -> 1
+    2     -> 1
+ 1  2     -> 2
+       3  -> 1
+    2  3  -> 2
+ 1  2  3  -> 3    
 
+         
+[1,1,1,1,1,1,2,2,2,2,2,3,3,3,3]
 
-# How to move pointers to get all the subarrays?
-  [1,  2,  3]
-  lr           -> 1
-   l   r       -> 12
-   l       r   -> 13  # when r outof index l++ and r = l
-       lr      -> 2
-       l   r   -> 23
-           lr  -> 23  # when r out of index, quit loop
-
-
-           
 '''
+
 from typing import List
-array = [1, 2, 3]
+from collections import defaultdict
+
+array = [1, 2, 3, 4, 5 , 6]
 # arr = [1, 2, 1]
 
-def medianForPair(a:int, b:int, subarr:List[int]) -> int:
-    return subarr[(a+b)//2]
+## This func returns the number of subarrays that have at most k distinct elements
+def atMostDistinct(k:int)-> int:
+  counter = 0
+   
+  hMap = defaultdict(int)
+  l, r = 0, 0
+
+  while r < len(array):
+      hMap[array[r]] += 1
+
+      # Shrink window if > k
+      while len(hMap) > k:
+        hMap[array[l]] -= 1
+        if hMap[array[l]] == 0:
+            del hMap[array[l]]
+        l += 1
+      
+      counter += (r-l) + 1
+      r += 1
     
+  return counter
 
 def findMedianOfSubarrayUniqueness(arr:List[int]) -> int:
-    subarr_medians = []
+  N = len(arr)
+  numberOfSubarrays = (N * (N+1)) // 2
+  medianIndex = (numberOfSubarrays-1) // 2 if numberOfSubarrays % 2 == 0 else numberOfSubarrays//2
+  maxMedianValue = len(set(array))
+  print("")
 
+  ## Binary search between 1 and maxMedianValue
+  ## lets say BS between 1 - 5
+  ## take 3, if this is median, the result, 
+  l, r = 1, maxMedianValue
 
-    for l in range(len(arr)): 
-      for r in range(l,len(arr)):
-        subarr_medians.append(medianForPair(l,r,arr))
+  while l <= r:
+    mid = l + (r-l)//2
 
-    return subarr_medians[len(subarr_medians)//2]
+    if atMostDistinct(mid-1) < medianIndex+1 <= atMostDistinct(mid):
+      return mid
+    elif medianIndex+1 <= atMostDistinct(mid-1):
+      r = mid -1
+    else:
+      l = mid +1   
 
+  return mid
+
+  
 print(findMedianOfSubarrayUniqueness(array))
