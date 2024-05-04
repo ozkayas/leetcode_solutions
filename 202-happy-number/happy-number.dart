dcee053492108cc3911e90d0cd432c670678fa29
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-05-04 20:55:34 +0300

    Time: 326 ms (78.36%) | Memory: 150 MB (47.01%) - LeetSync

--- 202-happy-number/happy-number.dart
+++ 202-happy-number/happy-number.dart
@@ -1,6 +1,5 @@
 class Solution {
-  final isSeen = Set();
-
+  // Two Pointers cozumu
   int sumOfSquares(int n){
     int sum = 0;
     while (n>0){
@@ -8,28 +7,53 @@ class Solution {
       sum += lastDigit * lastDigit;
       n = n ~/ 10;
     }
-
     return sum;
   }
 
-  bool isHappy(int n) {
-    isSeen.add(n);
-    int curNumber = n;
-
-    while(curNumber != 1){
-      //karelerini toplave yeni curNumber
-      curNumber = sumOfSquares(curNumber);
-
-      // Dongu kontorlu yapalim
-      if(isSeen.contains(curNumber)){
-        return false;
-      }{
-        isSeen.add(curNumber);
-      }
-
+  bool isHappy(int n){
+    //Edge case check
+    if(sumOfSquares(n) == 1){
+      return true;
     }
 
+    int fast = n;
+    int slow = n;
+
+    while (fast != 1){
+      fast = sumOfSquares(sumOfSquares(fast));
+      slow = sumOfSquares(slow);
+      if (fast == slow){
+        return false;
+      }
+    }
     return true;
-    
   }
+
+
+  /// HashMap , Set cozumu
+  // final isSeen = Set();
+
+  //   return sum;
+  // }
+
+  // bool isHappy(int n) {
+  //   isSeen.add(n);
+  //   int curNumber = n;
+
+  //   while(curNumber != 1){
+  //     //karelerini toplave yeni curNumber
+  //     curNumber = sumOfSquares(curNumber);
+
+  //     // Dongu kontorlu yapalim
+  //     if(isSeen.contains(curNumber)){
+  //       return false;
+  //     }{
+  //       isSeen.add(curNumber);
+  //     }
+
+  //   }
+
+  //   return true;
+    
+  // }
 }
