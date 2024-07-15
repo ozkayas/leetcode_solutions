8827ae9d3e74d5e45b2ba93c80b9f15e6dc96281
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-07-15 23:53:49 +0300

    Time: 1619 ms (29.87%) | Memory: 26.2 MB (31.17%) - LeetSync

--- 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.py
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.py
@@ -6,43 +6,63 @@
 #         self.right = right
 class Solution:
     def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
-        # adj map parent: [leftChild, rightChild]
-        adjMap = defaultdict(lambda : [None, None])
+        nodes = dict()
         children = set()
-        root = None
-
-        for desc in descriptions:
-            parent = desc[0]
-            child = desc[1]
-            isLeft = desc[2] == 1
-
-            if isLeft:
-                adjMap[parent][0] = child
-            else:
-                adjMap[parent][1] = child
+        
+        for parent, child, isLeft in descriptions:
+            if parent not in nodes: nodes[parent] = TreeNode(parent)
+            if child not in nodes: nodes[child] = TreeNode(child)
+            if isLeft: nodes[parent].left = nodes[child]
+            else: nodes[parent].right = nodes[child]
             children.add(child)
 
-        # find root node, if a node is a parent but not a child, then it is the root
-        for parent in adjMap.keys():
-            if parent not in children:
-                root = parent
-                break
-        # print("root", root)
+        for p, _, __ in descriptions:
+            if p not in children:
+                return nodes[p]
+
+
+
+
+
+
+        # First Solution
+        # # adj map parent: [leftChild, rightChild]
+        # adjMap = defaultdict(lambda : [None, None])
+        # children = set()
+        # root = None
+
+        # for desc in descriptions:
+        #     parent = desc[0]
+        #     child = desc[1]
+        #     isLeft = desc[2] == 1
+
+        #     if isLeft:
+        #         adjMap[parent][0] = child
+        #     else:
+        #         adjMap[parent][1] = child
+        #     children.add(child)
+
+        # # find root node, if a node is a parent but not a child, then it is the root
+        # for parent in adjMap.keys():
+        #     if parent not in children:
+        #         root = parent
+        #         break
+        # # print("root", root)
         
-        rootNode = TreeNode(root, None, None)
+        # rootNode = TreeNode(root, None, None)
 
-        bfsDeq = deque([rootNode])
+        # bfsDeq = deque([rootNode])
 
-        while bfsDeq:
-            curNode = bfsDeq.popleft()
-            if adjMap[curNode.val][0]:
-                curNode.left = TreeNode(adjMap[curNode.val][0])
-                bfsDeq.append(curNode.left)
-            if adjMap[curNode.val][1]:
-                curNode.right = TreeNode(adjMap[curNode.val][1])
-                bfsDeq.append(curNode.right)
+        # while bfsDeq:
+        #     curNode = bfsDeq.popleft()
+        #     if adjMap[curNode.val][0]:
+        #         curNode.left = TreeNode(adjMap[curNode.val][0])
+        #         bfsDeq.append(curNode.left)
+        #     if adjMap[curNode.val][1]:
+        #         curNode.right = TreeNode(adjMap[curNode.val][1])
+        #         bfsDeq.append(curNode.right)
 
-        return rootNode
+        # return rootNode
 
 
 
