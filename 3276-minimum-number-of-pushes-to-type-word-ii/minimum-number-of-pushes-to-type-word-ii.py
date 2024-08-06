997b1b0afb05d3df7c11018853e6763871be73cb
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-08-06 09:35:37 +0300

    Time: 221 ms (28.40%) | Memory: 17.7 MB (8.11%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -0,0 +1,36 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+
+        def tapValueFor(i: int) -> int:
+            if i <= 8:
+                return 1
+            elif i <= 16:
+                return 2
+            elif i <= 24:
+                return 3
+            else:
+                return 4
+        
+        letterMap = dict()
+        # Mapping letters 8 by 8, because 8 empty slots for each letter
+        mapCounter = 0
+
+        f = Counter(word)
+        fList = f.most_common() # sorted tuple of [(a,7),(b,5),...]
+
+        for letter, freq in fList:
+            mapCounter += 1
+            letterMap[letter] = tapValueFor(mapCounter)
+        
+        ans = 0
+        for ch in word:
+            ans += letterMap[ch]
+
+        return ans
+
+
+
+
+
+
+        
