23f8467920ff00a47f5ea324c7b65b49eb5e6197
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-09-03 20:19:32 +0300

    Time: 1746 ms (57.71%) | Memory: 16.9 MB (31.71%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,62 +1,13 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        N = len(nums)
+        dp = [1 for i in range(N)]
 
-        table = [0 for _ in range(len(nums))]
-        maxI = len(nums)-1         
-        table[maxI] = 1
-        i = maxI-1
-
-
-        while i >= 0:
-
-            maxSeq = 0
-            for j in range(i+1, maxI+1):
-                if nums[j] > nums[i]:
-                    maxSeq = max(maxSeq, table[j])
-            table[i] = maxSeq +1
-
-            i -= 1
-
-        return max(table)
-            
+        for i in range(1, N):
+            for j in range(i+1):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j]+1)
+        
+        return max(dp)
 
 
-
-
-
-
-
-'''  # RECURSIVE SOLUTION 
-
-
-        memo = {}
-        i, maxI =0, len(nums)-1
-        res = 0
-
-        def fn(i) -> int:
-            # print("i:{}, memo:{}".format(i,memo))
-            if i == maxI:
-                return 1
-
-            if i in memo:
-                return memo[i]
-
-            maxChild = 0
-
-            for j in range(i+1,maxI+1):
-                
-                if nums[i] < nums[j]:
-                    val = fn(j)
-                    maxChild = max(maxChild, val)
-
-            
-            memo[i] = 1 + maxChild
-            return 1 + maxChild
-
-
-        while i <= maxI:
-            res = max(res, fn(i))
-            i+=1
-
-        return res
-'''        
