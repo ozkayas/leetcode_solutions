4b9bd90c75ae1bf08b4d61dc693f929542c0cc86
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-09-06 13:06:35 +0300

    Time: 43 ms (9.78%) | Memory: 16.4 MB (97.27%) - LeetSync

--- 1119-robot-bounded-in-circle/robot-bounded-in-circle.py
+++ 1119-robot-bounded-in-circle/robot-bounded-in-circle.py
@@ -1,45 +1,16 @@
 class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
-        pos = [0,0]
-        direction = "N"
+        x = y = 0
+        dx, dy = 0 ,1
 
-        def move(inst:str):
-            nonlocal direction
-            if inst == "G":
-                if direction == "N":
-                    pos[1] += 1
-                elif direction == "S":
-                    pos[1] -= 1
-                elif direction == "W":
-                    pos[0] -= 1
-                elif direction == "E":
-                    pos[0] += 1                            
-            elif inst == "R":
-                if direction == "N":
-                    direction = "E"
-                elif direction == "S":
-                    direction = "W"
-                elif direction == "W":
-                    direction = "N"
-                elif direction == "E":
-                    direction = "S"
-            elif inst == "L":
-                if direction == "N":
-                    direction = "W"
-                elif direction == "S":
-                    direction = "E"
-                elif direction == "W":
-                    direction = "S"
-                elif direction == "E":
-                    direction = "N"
-
-        for inst in instructions:
-            move(inst)
-
-        if pos[0] == 0 and pos[1] == 0 and direction == "N":
-            return True
+        for ch in instructions:
+            if ch == "G" :
+                x += dx
+                y += dy
+            elif ch == "L": dx, dy = -dy, dx
+            else: dx, dy = dy, -dx 
         
-        if direction != "N":
-            return True
+        return x == y == 0 or (dx, dy) != (0, 1)
 
-        return False
+        
+  
