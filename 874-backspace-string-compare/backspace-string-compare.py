c9a98c12895c27681d4aa543ca79dd2e53fc9885
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-09-21 11:32:14 +0300

    Time: 35 ms (58.85%) | Memory: 16.7 MB (35.50%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.py
+++ 874-backspace-string-compare/backspace-string-compare.py
@@ -1,31 +1,36 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        nonMatchingIndex = -1
-        sLast, pLast = "", ""
-        sp, tp = len(s)-1, len(t)-1 
-
-        # Return the last valid character
-        def getLastValidCharIndex(s:str, i:int) -> int:
-            count = 0
-            while i >= 0:
-                if s[i] == "#":
-                    count -=1
+        # Returns the next nonHash, may also return the given index if it is already valid
+        def nextNonHashPos(i, word):
+            skip = 0
+            while i >= 0 and (word[i] == '#' or skip > 0):
+                if word[i] == '#':
+                    skip += 1
                 else:
-                    count += 1
-                if count > 0:
-                    return i 
+                    skip -= 1
                 i -= 1
             return i
 
-        # print(getLastValidCharIndex(s, sp))
+        s_pt, t_pt = len(s) - 1, len(t) - 1
 
-        sp = getLastValidCharIndex(s,sp)
-        tp = getLastValidCharIndex(t,tp)
-        while sp > -1 and tp > -1 and s[sp] == t[tp]:
-            sp = getLastValidCharIndex(s, sp-1)
-            tp = getLastValidCharIndex(t, tp-1)
-                
-        if sp == -1 and tp == -1:
-            return True
-        else:
+        while s_pt >= 0 and t_pt >= 0:
+            s_pt = nextNonHashPos(s_pt, s)
+            t_pt = nextNonHashPos(t_pt, t)
+
+            # If there is no char to compare, just quit
+            if s_pt < 0 or t_pt < 0:
+                break
+            
+            # If next chars do not match, just return False
+            if s[s_pt] != t[t_pt]:
+                return False
+            # next chars after backspace deleting matches, so shift index for the following left
+            # part of the strings
+            else:
+                s_pt -= 1
+                t_pt -= 1
+
+        if nextNonHashPos(s_pt, s) >= 0 or nextNonHashPos(t_pt, t) >= 0:
             return False
+        else:
+            return True
