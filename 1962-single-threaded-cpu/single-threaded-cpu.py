08839eb6deced4fc65254fc782f3962ebb3a857e
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-09-24 02:36:25 +0300

    Time: 1452 ms (57.27%) | Memory: 64.2 MB (73.62%) - LeetSync

---
+++ 1962-single-threaded-cpu/single-threaded-cpu.py
@@ -0,0 +1,37 @@
+class Solution:
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+
+        sortedTasks = sorted([(eTime, pTime, i) for i, [eTime, pTime] in enumerate(tasks)])
+        print(sortedTasks)
+        taskQueue = deque(sortedTasks)
+
+        # init minHeap with the first value
+        firstTask = taskQueue.popleft()
+        time = firstTask[0]
+        minHeap = [(firstTask[1],firstTask[2])]
+        heapq.heapify(minHeap)
+
+        ans = []
+        while taskQueue or minHeap:
+            # if heap is empty but taskQueue not, means that some taskwill begin in long future, but heap is processed
+            if taskQueue and not minHeap:
+                firstTask = taskQueue.popleft()
+                time = firstTask[0]
+                minHeap = [(firstTask[1],firstTask[2])]
+                heapq.heapify(minHeap)
+
+            # process first task in the heap, then update time and fill heap from taskqueue
+            pTime, idx = heapq.heappop(minHeap)
+            time += pTime 
+            ans.append(idx)
+            # print(f"found time{time}, pTime{pTime}, idx:{idx}")
+            # popleft from queue until time, and push into heap
+            while taskQueue and taskQueue[0][0] <= time:
+                task = taskQueue.popleft()
+                heapq.heappush(minHeap, (task[1], task[2]))
+
+
+        return ans
+
+
+        
