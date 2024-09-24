883356e2eb89105c14e476aa7e83755fd8807c88
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-09-25 02:06:10 +0300

    Time: 39 ms (72.90%) | Memory: 18.3 MB (90.37%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -7,21 +7,33 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
 
+        ### Solution 1 - easier way
         # Inordertravers - fill array= check if increasing order.
-        arr = []
-        def dfs(node):
-            if node.left:
-                dfs(node.left)
-            arr.append(node.val)
-            if node.right:
-                dfs(node.right)
+        # arr = []
+        # def dfs(node):
+        #     if node.left:
+        #         dfs(node.left)
+        #     arr.append(node.val)
+        #     if node.right:
+        #         dfs(node.right)
 
 
-        dfs(root)
-        # Check if the array is in strictly increasing order
-        for i in range(1, len(arr)):
-            if arr[i] <= arr[i - 1]:
-                return False
+        # dfs(root)
+        # # Check if the array is in strictly increasing order
+        # for i in range(1, len(arr)):
+        #     if arr[i] <= arr[i - 1]:
+        #         return False
 
-        return True
+        # return True
+
+
+        ## Solution 2
+        stack = [(root, -inf, inf)]
+        while stack:
+            node, lo, hi = stack.pop()
+            if lo < node.val < hi:
+                if node.left: stack.append((node.left, lo, node.val))
+                if node.right: stack.append((node.right, node.val, hi))
+            else: return False
+        return True  
         
