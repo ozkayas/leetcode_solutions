09bee40331c94ece3f6ebe3d767f1aeb58db3d49
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-09-26 22:17:08 +0300

    Time: 545 ms (81.34%) | Memory: 79.3 MB (13.42%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,64 +1,58 @@
 class Node:
-
-    def __init__(self, key:int, val:int):
-        self.val, self.key = val, key
-        self.prev = self.next = None
+    def __init__(self, key:int, value:int):
+        self.prev = None
+        self.next = None
+        self.key = key
+        self.val = value
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.hMap = {}
+        self.hMap = dict()
+        self.head = Node(0,0)
+        self.tail = Node(0,0)
         self.capacity = capacity
-        self.head = self.tail = Node(0,0)
         self.head.next = self.tail
         self.tail.prev = self.head
-
-    def add(self,node:Node):
-        # put it at the head
+    
+    def addNode(self, node):
         node.next = self.head.next
         node.prev = self.head
         self.head.next.prev = node
         self.head.next = node
 
-    def remove(self,node:Node):
-        # take it out from its position in the list
-        node.prev.next = node.next
-        node.next.prev = node.prev
+    def popNode(self, node):
+        pr = node.prev
+        nx = node.next
+        pr.next = nx
+        nx.prev =pr
 
     def get(self, key: int) -> int:
-        if key not in self.hMap:
-            return -1
-
-        self.remove(self.hMap[key])
-        self.add(self.hMap[key])
-        # print(self.hMap)
-
-        return self.hMap[key].val
-
+        if key in self.hMap:
+            cur = self.hMap[key]
+            self.popNode(cur)
+            self.addNode(cur)
+            return cur.val
+        
+        return -1
+        
 
     def put(self, key: int, value: int) -> None:
         if key in self.hMap:
             cur = self.hMap[key]
-            cur.val = value 
-            self.remove(cur)
-            self.add(cur)
+            cur.val = value
+            self.popNode(cur)
+            self.addNode(cur)
         else:
+            node = Node(key, value)
+            self.hMap[key] = node
+            self.addNode(node)
+            if len(self.hMap) > self.capacity:
+                nodeToPop = self.tail.prev
+                self.popNode(nodeToPop)
+                del self.hMap[nodeToPop.key]
 
+                 
 
-            if len(self.hMap) >= self.capacity:
-                leastNode = self.tail.prev
-                self.remove(leastNode)
-                self.hMap.pop(leastNode.key)
-
-            cur = Node(key, value)
-            self.hMap[key] = cur
-            self.add(cur)
-            # print(self.hMap)
 
         
-
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
