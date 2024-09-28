81d1d5f7d620811710791a824a25412153866ce4
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-09-28 22:01:31 +0300

    Time: 943 ms (26.19%) | Memory: 36.4 MB (34.76%) - LeetSync

--- 2055-describe-the-painting/describe-the-painting.py
+++ 2055-describe-the-painting/describe-the-painting.py
@@ -2,11 +2,13 @@ class Solution:
     def splitPainting(self, segments: List[List[int]]) -> List[List[int]]:
         N = len(segments)
         startingTime = float("inf")
-        curSet = set()
+        # curSet = set()
+        curSet = 0 ## lets try directly summing, instead of using set
+
         # {1:[5,8],4:[7]}- {4:[5], 7:[7,9]} 
         # using hashmap to read write O(1) time, 
         #a lternative could be sorting in an array and jumping on these 
-        # instead of swapping all timeline 1 by 1
+        # instead of sweeping all timeline 1 by 1
         starts, ends = defaultdict(list), defaultdict(list)
         for s, e, c in segments:
             starts[s].append(c)
@@ -23,16 +25,18 @@ class Solution:
         while True: ## We will continue until last paint end is handle
             if time in ends:
                 if lastOperationTime != time :
-                    ans.append([lastOperationTime, time, sum(curSet)])
+                    ans.append([lastOperationTime, time, (curSet)])
                 for color in ends[time]:
-                    curSet.remove(color)
+                    # curSet.remove(color)
+                    curSet -= color
                 lastOperationTime = time
                 endCounter += 1
             if time in starts:
                 if lastOperationTime != time and curSet:
-                    ans.append([lastOperationTime, time, sum(curSet)])
+                    ans.append([lastOperationTime, time, (curSet)])
                 for color in starts[time]:
-                    curSet.add(color)
+                    # curSet.add(color)
+                    curSet += color
                 lastOperationTime = time
 
             # Check if the simulation ended, last painting ended
