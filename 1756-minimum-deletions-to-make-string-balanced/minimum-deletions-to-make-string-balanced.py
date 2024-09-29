2e21f4ca2d26755098572fde424eab777f94ce8d
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-09-29 22:36:58 +0300

    Time: 247 ms (76.36%) | Memory: 18 MB (42.59%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
@@ -1,31 +1,15 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
-        bPref, aSuf = [], []
-        
-        cnt = 0
+        st = []
+        deleteCounter = 0
+
         for ch in s:
-            bPref.append(cnt)
-            if ch == "b": cnt += 1
-        cnt = 0
-        for ch in s[::-1]:
-            aSuf.append(cnt)
-            if ch == "a": cnt += 1
-        aSuf.reverse()
+            if st and st[-1] == "b" and ch == "a":
+                deleteCounter += 1
+                st.pop()
+            else:
+                st.append(ch)
+            
+        return deleteCounter
+
         
-        ans = len(s)
-        for i in range(len(s)):
-            ans = min(ans, bPref[i]+aSuf[i])
-        
-        return ans
-
-'''
-Approach 1: prefix loop then update variable. 
-N = 8
- 0 0 0 1 1 2 3 3   - b prefix
-"a a b a b b a b"
- 3 2 2 1 1 1 0 0
-
-
-
-
-'''
