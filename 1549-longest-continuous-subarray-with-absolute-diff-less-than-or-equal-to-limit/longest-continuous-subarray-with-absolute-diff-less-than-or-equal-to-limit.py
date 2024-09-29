e5535fad1707f3326a894c7c39c5895121a0e6cc
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-09-29 21:07:36 +0300

    Time: 1260 ms (7.34%) | Memory: 28.3 MB (28.80%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,48 +1,27 @@
+from sortedcontainers import SortedList
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        # init deques 
-        minDeq = deque([nums[0]])
-        maxDeq = deque([nums[0]])
-
-
-        # returns ans diff of min & max
-        def isDiffInLimit() -> bool:
-            delta = abs(minDeq[0] - maxDeq[0])
-            return delta <= limit
-        
-        # update deques when window is shrinked or expanded
-        def updateDeques(n:int, operation: str):
-            if operation == "expand":
-                while minDeq and n < minDeq[-1]:
-                    minDeq.pop()
-                minDeq.append(n)
-                while maxDeq and n > maxDeq[-1]:
-                    maxDeq.pop()
-                maxDeq.append(n)
-            # shrinking window
-            else:
-                if n == minDeq[0]:
-                    minDeq.popleft()
-                if n == maxDeq[0]:
-                    maxDeq.popleft()
-
-
         l, r = 0, 0
-        maxSubLen = 0
+        sl = SortedList()
+        ans = 0
 
         while r < len(nums):
-
-            if isDiffInLimit():
-                maxSubLen = max(maxSubLen, (r-l+1))
-                r += 1
-                # update min, max while expanding the window
-                if r < len(nums):
-                   updateDeques(nums[r],"expand") 
-            else:
-                updateDeques(nums[l],"shrink")
+            sl.add(nums[r])
+            # Check if window is valid or not
+            mn, mx = sl[0], sl[-1]
+            if abs(mx-mn) <= limit:
+                ans = max(ans, (r-l+1))
+            
+            # Shrink window until reaching a valid state  
+            while abs(mx-mn) > limit:
+                sl.remove(nums[l])
                 l += 1
+                # Update min and max since `l` was modified
+                mn, mx = sl[0], sl[-1] if sl else (0, 0)
 
-        return maxSubLen
+            r += 1
+        
+        return ans
 
 
         
