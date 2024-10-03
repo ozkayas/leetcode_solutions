cf5c75252a19ad94ea4fd2eea2ed7b5c7d797942
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-03 08:21:51 +0300

    Time: 606 ms (34.54%) | Memory: 33.2 MB (61.90%) - LeetSync

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -1,57 +1,23 @@
 class Solution:
     def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
-            adjList = [[] for i in range(n)]
-            inDegree = [0 for i in range(n)]
 
-            for u, v in edges: # u -> v
-                adjList[u].append(v)
-                inDegree[v] += 1
+        ancestors = [[] for _ in range(n)]
+        adjList = defaultdict(list)
 
-            
-            ans = [[] for i in range(n)]
+        for from_node, to_node in edges:
+            adjList[from_node].append(to_node)
 
-            ## Fill topological sorting starting nodes
-            nodes_with_zero_indegree = deque()
-            for i in range(n):
-                if inDegree[i] == 0:
-                    nodes_with_zero_indegree.append(i)
+        def explore(node, parentToAdd):
+            for child in adjList[node]:
+                if ancestors[child] and ancestors[child][-1] == parentToAdd:
+                    continue
+                else:
+                    ancestors[child].append(parentToAdd)
+                    # print(f"visiting {node}, child:{child}, added ancestor{ancestors}")
+                    explore(child, parentToAdd)
 
-            topologicalOrder = []
-            while nodes_with_zero_indegree:
-                curNode = nodes_with_zero_indegree.popleft()
-                topologicalOrder.append(curNode)
+                
+        for node in range(n):
+            explore(node, node)
 
-                for neighbor in adjList[curNode]:
-                    inDegree[neighbor] -= 1
-                    if inDegree[neighbor] == 0:
-                        nodes_with_zero_indegree.append(neighbor)
-            # Initialize the result list and set list for storing ancestors
-            ancestors_list = [[] for _ in range(n)]
-            ancestors_set_list = [set() for _ in range(n)]
-            
-            # Fill the set list with ancestors using the topological order
-            for node in topologicalOrder:
-                for neighbor in adjList[node]:
-                    # Add immediate parent, and other ancestors.
-                    ancestors_set_list[neighbor].add(node)
-                    ancestors_set_list[neighbor].update(ancestors_set_list[node])
-
-            # Convert sets to lists and sort them
-            for i in range(n):
-                ancestors_list[i].extend(ancestors_set_list[i])
-                ancestors_list[i].sort()
-
-            return ancestors_list
-
-
-'''
-adj List
-       0: 3,4
-       1: 3
-       2: 4, 7
-       3: 5,6,7 
-       4: 6
-       5:
-       6:
-       7: 
-''' 
+        return ancestors
