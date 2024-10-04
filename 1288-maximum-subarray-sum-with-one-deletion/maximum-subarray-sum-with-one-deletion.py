4a7b4a8abdbe88a4e7b3d6a35253a08ad6a777ba
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-04 19:27:08 +0300

    Time: 212 ms (27.79%) | Memory: 28.7 MB (29.01%) - LeetSync

--- 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.py
+++ 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.py
@@ -1,43 +1,27 @@
 class Solution:
     def maximumSum(self, arr: List[int]) -> int:
-        # If there is only 1 element, we can not delete anything and no subarray, just return it
-        if len(arr) == 1: return arr[0]
-
-        # explanation https://www.youtube.com/watch?v=AxkYOnS-hjs
-        # Kadane's algo from both sides
         N = len(arr)
-        max_left, max_right = [], []
+        if N == 1: return arr[0]
 
-        # Fill max_left
-        max_so_far = float("-inf")
-        for n in arr:
-            max_so_far = max(max_so_far + n, n)
-            max_left.append(max_so_far)
-        # print(max_left)
+        forward, backward = [0] * N, [0] * N
+        forward[0] = arr[0]
+        backward[-1] = arr[-1]
 
-        # Fill max_right
-        max_so_far = float("-inf")
-        for n in reversed(arr):
-            max_so_far = max(max_so_far + n, n)
-            max_right.append(max_so_far)
-        max_right.reverse()
-        # print(max_right)
+        for i in range(1, N): 
+            forward[i] = max(forward[i-1] + arr[i], arr[i])
 
-        # Look for negatives and check what happens if we delete it. by looking leftpart and rightpart 
-        max_after_deletion = float("-inf")
-        for i,n in enumerate(arr):
-            if n < 0:
-                left_part_max = max_left[i-1] if (i-1) >= 0 else 0
-                right_part_max = max_right[i+1] if (i+1) < N else 0
-                max_after_deletion = max(max_after_deletion, left_part_max + right_part_max)
+        for i in range(N-2, -1, -1):
+            backward[i] = max(backward[i+1] + arr[i], arr[i])
 
+        max_no_deletion = max(forward)
+        # calculate with skipping for each num
+        max_one_deletion = -pow(10, 5)
 
-        max_of_sub_without_deletion = max(max_right)
-        return max(max_after_deletion, max_of_sub_without_deletion)
-            
-
-
-            
-
-
+        for i in range(1,N-1):
+            max_cur_deleted = forward[i-1] + backward[i+1]
+            max_one_deletion = max(max_one_deletion, max_cur_deleted)
         
+        return max(max_no_deletion, max_one_deletion)
+
+
+
