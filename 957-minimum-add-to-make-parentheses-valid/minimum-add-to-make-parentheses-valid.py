8b48beb2cc364322787c2fd489e604088e124860
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-06 11:16:34 +0300

    Time: 37 ms (43.48%) | Memory: 16.3 MB (93.96%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        o, c = 0, 0
+        for ch in s:
+            if ch == "(":
+                o += 1
+            else:
+                if o > 0:
+                    o -= 1
+                else:
+                    c += 1
+        
+        return o + c
+        
