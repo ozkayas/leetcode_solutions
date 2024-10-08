2dfc0e7514c5f786f9ab3ec2172ab4f28dff5fb4
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-08 10:34:51 +0300

    Time: 27 ms (93.26%) | Memory: 16.6 MB (37.96%) - LeetSync

--- 670-maximum-swap/maximum-swap.py
+++ 670-maximum-swap/maximum-swap.py
@@ -3,6 +3,11 @@ class Solution:
         # To ease swap and early return
         numAsList = [int(ch) for ch in str(num)]
         indexTable = defaultdict(list)
+
+        def intParse(l:list[int]) -> int:
+            return int("".join([str(n) for n in l]))
+
+
         # Filling indexes from back, for easier popping when an element is used
         for i in range(len(numAsList)-1, -1, -1):
             indexTable[numAsList[i]].append(i)
@@ -13,11 +18,11 @@ class Solution:
                 if x in indexTable and indexTable[x][0] > i:
                     #swap and return
                     numAsList[i], numAsList[indexTable[x][0]] = numAsList[indexTable[x][0]], numAsList[i] 
-                    return int("".join([str(n) for n in numAsList]))
+                    return intParse(numAsList)
             # can not swap so remove this from the table, because it is used now
             indexTable[n].pop()
             if not indexTable[n]: del indexTable[n]
 
-        return int("".join([str(n) for n in numAsList]))
+        return intParse(numAsList)
 
         
