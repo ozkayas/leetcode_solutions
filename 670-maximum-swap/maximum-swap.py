a90002e5f32a71c2c79741348af62d855bf1ea70
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-08 10:31:11 +0300

    Time: 27 ms (93.26%) | Memory: 16.6 MB (8.38%) - LeetSync

---
+++ 670-maximum-swap/maximum-swap.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        # To ease swap and early return
+        numAsList = [int(ch) for ch in str(num)]
+        indexTable = defaultdict(list)
+        # Filling indexes from back, for easier popping when an element is used
+        for i in range(len(numAsList)-1, -1, -1):
+            indexTable[numAsList[i]].append(i)
+
+        for i, n in enumerate(numAsList):
+            # does table has a bigger key than this at a later index?
+            for x in range(9, n, -1):
+                if x in indexTable and indexTable[x][0] > i:
+                    #swap and return
+                    numAsList[i], numAsList[indexTable[x][0]] = numAsList[indexTable[x][0]], numAsList[i] 
+                    return int("".join([str(n) for n in numAsList]))
+            # can not swap so remove this from the table, because it is used now
+            indexTable[n].pop()
+            if not indexTable[n]: del indexTable[n]
+
+        return int("".join([str(n) for n in numAsList]))
+
+        
