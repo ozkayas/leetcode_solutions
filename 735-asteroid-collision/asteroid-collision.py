09cc2aa67ca271496e2ec4b21cb37d3d53ea93fc
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-13 17:06:49 +0300

    Time: 79 ms (75.11%) | Memory: 18 MB (15.24%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,19 +1,21 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         res = []
-        for a in asteroids:
-            collision_occurred = False
-            while res and a < 0 < res[-1]:
-                if res[-1] < -a:
+        for curr in asteroids:
+            curr_survived = True
+            # Collusion case, cur moving left and topof the stack to right
+            # if astreoid survives it continue crashing to elements on the stack !!!
+            while res and curr < 0 < res[-1]:
+                if res[-1] < -curr:
+                    res.pop() #explode last one
+                elif res[-1] == -curr:
                     res.pop()
-                elif res[-1] == -a:
-                    res.pop()
-                    collision_occurred = True
-                    break
+                    curr_survived = False
+                    break # We did not survive, so break the while loop
                 else:
-                    collision_occurred = True
-                    break
-            if not collision_occurred:
-                res.append(a)
+                    curr_survived = False
+                    break # We did not survive, so break the while loop
+            if curr_survived:
+                res.append(curr)
         return res
  
