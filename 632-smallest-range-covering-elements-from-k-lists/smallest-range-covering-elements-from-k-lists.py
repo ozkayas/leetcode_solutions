758493b2ce2b0e4fa5e145e070a2954cea9d0e7e
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-13 22:58:16 +0300

    Time: 210 ms (23.58%) | Memory: 24.5 MB (12.96%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.py
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.py
@@ -13,6 +13,7 @@ class Solution:
                 all.append(Data(n,i))
 
         all.sort()
+
         # print(all)
         # buckets that exist in the current window
         bucketMap = defaultdict(int)
@@ -25,11 +26,14 @@ class Solution:
             bucketMap[right.bucketId] += 1
             while len(bucketMap) >= numOfBuckets: # Found a valid window, process it, then shrink window
                 left = all[l]
+                # If a smaller range is found:
                 if (right.val - left.val) < (minRange[1].val - minRange[0].val):
                     minRange = [left, right]
+                # Process bucketMap    
                 bucketMap[left.bucketId] -= 1
                 if bucketMap[left.bucketId] == 0:
                     del bucketMap[left.bucketId] 
+
                 l += 1
             r += 1
         return [minRange[1][0], minRange[0][0]]
