901f12f5c435f564af6898ad91b87f4b3b46bfa2
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-13 12:53:31 +0300

    Time: 1532 ms (28.84%) | Memory: 65.3 MB (57.55%) - LeetSync

--- 1962-single-threaded-cpu/single-threaded-cpu.py
+++ 1962-single-threaded-cpu/single-threaded-cpu.py
@@ -1,35 +1,38 @@
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        Task = namedtuple("Task", ["enqTime", "processTime", "id"])
+        # Helper method
+        def initShortestTaskMinHeap(taskQueue: deque) -> list:
+            firstTask = taskQueue.popleft()
+            minHeap = [(firstTask.processTime, firstTask.id)]
+            return minHeap
 
-        sortedTasks = sorted([(eTime, pTime, i) for i, [eTime, pTime] in enumerate(tasks)])
-        # print(sortedTasks)
+        def fillMinHeapWithAvailableTasks(minHeap:list, taskQueue:deque):
+            while taskQueue and taskQueue[0].enqTime <= time:
+                task = taskQueue.popleft()
+                heapq.heappush(minHeap, (task.processTime, task.id))
+
+        sortedTasks = sorted([Task(eTime, pTime, i) for i, [eTime, pTime] in enumerate(tasks)])
+        # All the tasks: when simulation time ticks, will take tasks from this queue and add to the procesMinHeap 
         taskQueue = deque(sortedTasks)
-
-        # init minHeap with the first value
-        firstTask = taskQueue.popleft()
-        time = firstTask[0]
-        minHeap = [(firstTask[1],firstTask[2])]
-        heapq.heapify(minHeap)
+        time = taskQueue[0].enqTime
+        # Holds tasks with shortest process time enqued in the system, and waiting to be processes, CPU will select from here. not taskQueue
+        minHeap = initShortestTaskMinHeap(taskQueue)
 
         ans = []
         while taskQueue or minHeap:
-            # if heap is empty but taskQueue not, means that some taskwill begin in long future, but heap is processed
+            # if heap is empty but taskQueue not, means that some taskwill begin in long future, 
+            # Fast forward the simulation to the next tasks enque time and re-init minHeap
             if taskQueue and not minHeap:
-                firstTask = taskQueue.popleft()
-                time = firstTask[0]
-                minHeap = [(firstTask[1],firstTask[2])]
-                heapq.heapify(minHeap)
+                time = taskQueue[0].enqTime
+                minHeap = initShortestTaskMinHeap(taskQueue)
 
             # process first task in the heap, then update time and fill heap from taskqueue
             pTime, idx = heapq.heappop(minHeap)
             time += pTime 
             ans.append(idx)
-            # print(f"found time{time}, pTime{pTime}, idx:{idx}")
-            # popleft from queue until time, and push into heap
-            while taskQueue and taskQueue[0][0] <= time:
-                task = taskQueue.popleft()
-                heapq.heappush(minHeap, (task[1], task[2]))
-
+            # Grab the tasks that are now ready to be processed, and push into heap so CPU can select the one with shortest process time
+            fillMinHeapWithAvailableTasks(minHeap, taskQueue) 
 
         return ans
 
