e7764b7d544f97b432463d067d33c1f6ee6ac7b9
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-13 22:44:30 +0300

    Time: 214 ms (21.68%) | Memory: 24.5 MB (12.96%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.py
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.py
@@ -1,14 +1,16 @@
 class Solution:
     def smallestRange(self, nums: List[List[int]]) -> List[int]:
+        Data = namedtuple("Data", ["val","bucketId"])
         numOfBuckets = len(nums)
         # Sliding Window:
-        # Validity of the window => len(set) == len(window)
+        # Validity of the window => len(bucketMapInWindow) == numOfBuckets
 
-        # put all in a sorted list as tuples (value, bucketNo)
+
+        # put all in a sorted list as tuples Data(val, bucketId)
         all = []
         for i in range(len(nums)):
             for n in nums[i]:
-                all.append((n,i))
+                all.append(Data(n,i))
 
         all.sort()
         # print(all)
@@ -19,15 +21,15 @@ class Solution:
 
         l, r = 0, 0
         while r < len(all):
-            bucketMap[all[r][1]] += 1
+            right = all[r]
+            bucketMap[right.bucketId] += 1
             while len(bucketMap) >= numOfBuckets: # Found a valid window, process it, then shrink window
-                # print((all[l][0] , all[r][0]))
-                if (all[r][0] - all[l][0]) < (minRange[1][0] - minRange[0][0]):
-                    minRange = [all[l], all[r]]
-                bucketMap[all[l][1]] -= 1
-                if bucketMap[all[l][1]] == 0:
-                    del bucketMap[all[l][1]] 
+                left = all[l]
+                if (right.val - left.val) < (minRange[1].val - minRange[0].val):
+                    minRange = [left, right]
+                bucketMap[left.bucketId] -= 1
+                if bucketMap[left.bucketId] == 0:
+                    del bucketMap[left.bucketId] 
                 l += 1
             r += 1
         return [minRange[1][0], minRange[0][0]]
-        
