e2e9090605d842af64bf015dfbae2fc23b9c4d03
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-15 23:31:17 +0300

    Time: 131 ms (44.88%) | Memory: 31.1 MB (52.89%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -1,52 +1,38 @@
+class TrieNode:
+    def __init__(self):
+        self.children = dict()
+        self.isWord = False
+
 class Trie:
 
     def __init__(self):
         self.root = TrieNode()
+        
 
     def insert(self, word: str) -> None:
-        curNode = self.root
+        cur = self.root
+        for w in word:
+            if w not in cur.children:
+                cur.children[w] = TrieNode()
 
-        for i in range(len(word)):
-            cur = word[i]
-            idx_cur = ord(cur) - ord('a')
-
-            # If no index to insert this character
-            if curNode.children[idx_cur] == None:
-                curNode.children[idx_cur] = TrieNode()
-            
-            # Switch to next node
-            curNode = curNode.children[idx_cur]
-
-            if i == len(word)-1:
-                curNode.isWord = True
-        
+            cur = cur.children[w]
+        cur.isWord = True
 
     def search(self, word: str) -> bool:
-        curNode = self.root
-        
+        cur = self.root
         for w in word:
-            idx = ord(w)-ord("a")
-            node_for_w = curNode.children[idx]
-
-            if node_for_w == None: # This letter not exists so return False
+            if w not in cur.children:
                 return False
-
-            curNode = node_for_w
+            cur = cur.children[w]
+        return cur.isWord
         
-        return curNode.isWord
-            
+
     def startsWith(self, prefix: str) -> bool:
-        curNode = self.root
-        
+        cur = self.root
         for w in prefix:
-            idx = ord(w)-ord("a")
-            node_for_w = curNode.children[idx]
-
-            if node_for_w == None: # This letter not exists so return False
+            if w not in cur.children:
                 return False
-
-            curNode = node_for_w
-        
+            cur = cur.children[w]
         return True
         
 
@@ -56,8 +42,3 @@ class Trie:
 # obj.insert(word)
 # param_2 = obj.search(word)
 # param_3 = obj.startsWith(prefix)
-
-class TrieNode:
-    def __init__(self):
-        self.isWord = False
-        self.children = [None for _ in range(26)]
