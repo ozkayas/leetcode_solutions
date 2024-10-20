0b6b19d3a0fb859735d216587dbbe7a5507ca6f2
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-20 19:45:47 +0300

    Time: 315 ms (65.94%) | Memory: 16.6 MB (78.60%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,43 +1,40 @@
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
+    # search to both sides from the input index and returns the found palindrome
+    def search(self,i:int, s:str) -> str:
+        possiblePalindrome = ''
+        # search for 1 letter in the center and to both sides "aaxaa" 
+        #                                                        ^
+        l, r = i, i
+        while l-1 >= 0 and r+1 < len(s) and s[l-1] == s[r+1]:
+                l -= 1
+                r += 1
+        
+        possiblePalindrome = s[l:r+1]
 
-        # search to both sides from the input index and returns the found palindrome
-        def search(i:int, s:str) -> str:
-            possiblePalindrome = ''
-            # search for 1 letter in the center and to both sides "aaxaa" 
-            #                                                        ^
-            l, r = i, i
+        #search for 2 twin letters in the center, ie: "aaxxaa"
+        #                                                ^
+        if i+1 < len(s) and s[i] == s[i+1]:
+            l, r = i, i+1
             while l-1 >= 0 and r+1 < len(s) and s[l-1] == s[r+1]:
                     l -= 1
                     r += 1
-            
-            possiblePalindrome = s[l:r+1]
 
-            #search for 2 twin letters in the center, ie: "aaxxaa"
-            #                                                ^
-            if i+1 < len(s) and s[i] == s[i+1]:
-                l, r = i, i+1
-                while l-1 >= 0 and r+1 < len(s) and s[l-1] == s[r+1]:
-                        l -= 1
-                        r += 1
+            # we found a longer candidate
+            if len(s[l:r+1]) > len(possiblePalindrome):
+                possiblePalindrome = s[l:r+1]
 
-                # we found a longer candidate
-                if len(s[l:r+1]) > len(possiblePalindrome):
-                    possiblePalindrome = s[l:r+1]
+        return possiblePalindrome
 
-            return possiblePalindrome
 
-        # print(search(0,s))
-        # print(search(1,s))
-        maxSoFar = ""
-        
+
+    def longestPalindrome(self, s: str) -> str:
+        ans = ""
         for i in range(len(s)):
-            curPalindrome = search(i, s)
-            if len(curPalindrome) > len(maxSoFar):
-                maxSoFar = curPalindrome
+            pal = self.search(i,s)
+            if len(pal) > len(ans):
+                ans = pal
 
-        return maxSoFar
+        return ans
 
-
-
+    
         
