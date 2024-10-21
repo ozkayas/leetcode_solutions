e7d1f286b078bef7201495ac6a512f420bfe01a2
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-21 22:31:28 +0300

    Time: 3 ms (99.16%) | Memory: 16.6 MB (83.67%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,25 +5,23 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        newHead = ListNode()
-        p3 = newHead
-        p1, p2 = l1, l2
-
-        # continue while we still have any of the lists to be processed
+        dummy = ListNode()
+        p1, p2, p3 = l1, l2, dummy
         carry = 0
-        while p1 or p2 or carry:
 
+        while p1 or p2 or carry:
             if p1:
                 carry += p1.val
                 p1 = p1.next
             if p2:
                 carry += p2.val
                 p2 = p2.next
-
-            # create new Node and add to newList, and move pointer to this node
-            node = ListNode(carry % 10)
-            p3.next = node
-            p3 = node
-            carry = carry // 10
+            
+            newNode = ListNode(carry % 10)
+            p3.next = newNode
+            p3 = p3.next
+            carry //= 10
         
-        return newHead.next
+        return dummy.next
+            
+        
