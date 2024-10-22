9b055b1847c3d916d7b669b8bbaee6fa2ddddd81
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-22 09:20:45 +0300

    Time: 55 ms (99.69%) | Memory: 52.8 MB (17.40%) - LeetSync

--- 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.py
+++ 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.py
@@ -6,42 +6,24 @@
 #         self.right = right
 class Solution:
     def kthLargestLevelSum(self, root: Optional[TreeNode], k: int) -> int:
+        bfs = deque([root])
+        sums = []
 
-        # will use a minHeap at size k to dynamicly hold kth max value
-        minHeap = []
-        heapq.heapify(minHeap)
+        levelProcessed = 0
+        levelSum = 0
+        while bfs:
+            for _ in range(len(bfs)):
+                cur = bfs.popleft()
+                levelSum += cur.val 
+                if cur.left: bfs.append(cur.left)
+                if cur.right: bfs.append(cur.right)
+            levelProcessed += 1
+            sums.append(levelSum)
+            levelSum = 0
 
-        # bfs to track level order transversal
-        # Will hold sum of each level. root is level:0 {0:5, 1:17, 2: 13, ..}
-        levelSum = defaultdict(int)
-        st = deque([(root, 0)])
+        sums.sort()
+        return sums[-k] if k <= len(sums) else -1
 
-        # check and update heap, if reached a new level, delete level to save space
-        def updateHeap(level: int):
-            if levelSum[level] == 0 and level > 0: # level>0 is needed not to include levelSum[-1]
-                heapq.heappush(minHeap, levelSum[level-1])
-                while len(minHeap) > k:
-                    heapq.heappop(minHeap)
-                del levelSum[level-1]
-
-
-        lastLevelVisited = 0
-        while st:
-            node, level = st.popleft()
-            lastLevelVisited = level
-            # Lets check if we reached a new level, and if so push into heap the prev level
-            updateHeap(level)
-            levelSum[level] += node.val
-
-            if node.left:
-                st.append((node.left, level+1))
-            if node.right:
-                st.append((node.right, level+1))
-        ## update heap 1 more time after bfs is done
-        updateHeap(lastLevelVisited+1)
-
-        return minHeap[0] if len(minHeap) == k else -1
-
-
+            
 
         
