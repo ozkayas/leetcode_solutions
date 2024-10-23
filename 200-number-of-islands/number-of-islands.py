3e58f6f111e405d007aef6c6939f146f7d258739
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-23 12:54:28 +0300

    Time: 225 ms (89.64%) | Memory: 19.1 MB (47.01%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,18 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         R, C = len(grid), len(grid[0])
-        ans = 0
+        islands = 0
 
-        def dfs(r,c):
+        # sink islands using bfs to directions
+        def sink(r, c):
             grid[r][c] = "0"
-            directions = [(r+1,c),(r-1,c),(r,c-1),(r,c+1)]
+
+            directions = [(r-1,c),(r+1,c),(r,c-1),(r,c+1)]
             for rr,cc in directions:
                 if 0 <= rr < R and 0 <= cc < C and grid[rr][cc] == "1":
-                    dfs(rr,cc)
+                    sink(rr,cc)
+        
 
         for r in range(R):
             for c in range(C):
                 if grid[r][c] == "1":
-                    ans += 1
-                    dfs(r,c)
-        return ans
+                    sink(r,c)
+                    islands += 1
+        
+        return islands
+        
