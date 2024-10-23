6521ba8bfa8f1bc33e5936afeb53dee5027be1bc
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-23 12:41:43 +0300

    Time: 48 ms (98.99%) | Memory: 21.1 MB (15.16%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,48 +1,33 @@
 class Solution:
     def calculate(self, s: str) -> int:
-
-        def handleOp(prev, op, cur) -> str:
-            # print("operation: ", prev,op,cur)
-            match op:
-                case "*":
-                    return str(int(prev)*int(cur))
-                case "/":
-                    res = int(prev) / int(cur) 
-                    return math.ceil(res) if res < 0 else math.floor(res)
-                case "-":
-                    return str(int(prev)-int(cur))
-                case "+":
-                    return str(int(prev)+int(cur))
-
-        st = []
-        # First pass
-        curNum = []
-        for c in s:
-            if c == " ": continue
-            if c.isdigit():
-                curNum.append(c)
+        s = s + "." # Adding a dummy char so the last digit will be processed
+        def handleOp(prev, nxt, op):
+            if op == "*":
+                return prev * nxt
             else:
-                st.append("".join(curNum)) # append num
-                st.append(c) # append operator
-                curNum.clear()
-        if curNum: st.append("".join(curNum))
-        # print(st)
+                val = prev/nxt
+                return math.ceil(val) if val < 0 else math.floor(val) 
 
-        # second pass
-        # st only holds values this time, not operators
-        ans = []
         op = "+"
-        for item in st:
-            if item.isdigit():
-                match op:
-                    case "+":
-                        ans.append(int(item))
-                    case "-":
-                        ans.append(-int(item))
-                    case _:
-                        prev = ans.pop()
-                        ans.append(int(handleOp(prev,op,item)))
-            else: # item is an operator
-                op = item
-        # print(ans)
-        return sum(ans)
+        st = []
+
+        temp = 0
+        for ch in s:
+            if ch == " ": continue
+            if ch.isdigit():
+                temp = temp*10 + int(ch)
+            else:
+                # see a new operator
+                if op in ["+", "-"]:
+                    st.append(temp if op == "+" else -temp)
+                else:
+                    st.append(handleOp(st.pop(),temp, op))
+                op = ch
+                temp = 0
+        
+        return sum(st)
+
+
+
+
+        
