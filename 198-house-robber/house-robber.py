9114a8cab89a99f028b397867c5397195560c46c
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-25 07:45:46 +0300

    Time: 0 ms (100.00%) | Memory: 16.6 MB (43.65%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,13 +1,22 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        memo = {}
+        if len(nums) < 3: return max(nums)
+        dp = [0 for _ in range(len(nums))]
+        dp[0]= nums[0]
+        dp[1]= nums[1]
+        
+        for i in range(2, len(nums)):
+            prev = dp[i-1]-nums[i-1]
+            prevprev = dp[i-2]
+            dp[i] = max(prev,prevprev) + nums[i]
+        
+        return max(dp[-1],dp[-2])
 
-        def fn(i):
-            if i < 0: return 0
-            if i not in memo:
-                memo[i] =  max(nums[i]+fn(i-2), fn(i-1))
-            return memo[i]
-            
 
+"""
+[2,7, 9, 1, 3,  1] 
+ 2 7  11  
 
-        return fn(len(nums)-1)     
+ 2 1 1 2
+ 2 1 
+ """
