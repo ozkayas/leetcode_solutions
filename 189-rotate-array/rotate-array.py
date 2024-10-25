7ba9b81a536832456e3d5d5f0961c4453e939c93
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-25 23:23:26 +0300

    Time: 5 ms (95.47%) | Memory: 24.8 MB (90.08%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,52 +1,17 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        k = k % len(nums)
-        # l,r = 0 , len(nums)-1
-
-        # while l<r:
-        #     nums[l],nums[r] = nums[r],nums[l]
-        #     l += 1
-        #     r -= 1
+        """
+        Do not return anything, modify nums in-place instead.
+        """
         
-        # l, r = 0, k-1
-        # while l<r:
-        #     nums[l],nums[r] = nums[r],nums[l]
-        #     l += 1
-        #     r -= 1
+        def reverse(i,j):
+            while i < j:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j -= 1
+        k %= len(nums)
+        reverse(0, len(nums)-1)
+        reverse(0, k-1)
+        reverse(k, len(nums)-1)
 
-        # l, r = k, len(nums)-1
-        # while l<r:
-        #     nums[l],nums[r] = nums[r],nums[l]
-        #     l += 1
-        #     r -= 1
-
-
-        print(nums)
-        def rev(l,r):
-            while l<r:
-                nums[l],nums[r] = nums[r],nums[l]
-                l += 1
-                r -= 1
-
-        rev(0 , len(nums)-1)
-        rev(0, k-1)
-        rev(k, len(nums)-1)
-
-
-
-
-
-### Solutiion 1
-        # res = []
-        # i = 0
-        # while i < len(nums):
-        #     ii = i + len(nums) - k  #i=0 ii=4
-        #     ii = ii % len(nums)
-        #     # print(ii)
-        #     res.append(nums[ii])
-        #     i += 1
         
-        # for i in range(len(nums)):
-        #     nums[i] = res[i]
-
-            
