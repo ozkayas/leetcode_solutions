21b707a75bfba218398e3615c1b8a93a057928c4
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-25 08:49:58 +0300

    Time: 17 ms (98.79%) | Memory: 29.6 MB (93.10%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -1,18 +1,13 @@
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
         folder.sort()
-
         ans = []
+
         for f in folder:
-            isSub = False
-            for seen in ans:
-                if f.startswith(seen+"/"):
-                    isSub = True
-                    break
-            if not isSub:
+            # Eğer ans boşsa veya yeni klasör, önceki klasörün bir alt klasörü değilse ekle
+            if not ans or not f.startswith(ans[-1] + '/'):
                 ans.append(f)
 
-
-        return list(ans)
+        return ans
 
         
