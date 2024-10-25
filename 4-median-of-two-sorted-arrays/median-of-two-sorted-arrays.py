0dfce36a2ad6c92075b4c7c692fd73f79bbf74ac
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-25 14:38:11 +0300

    Time: 0 ms (100.00%) | Memory: 17 MB (29.29%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,43 +1,38 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        if len(nums1) > len(nums2): nums1, nums2 = nums2, nums1
-        m, n = len(nums1), len(nums2)
-        lo, hi = 0, m
-
-        while lo <= hi:
-            mid = (lo + hi)//2
-            k = (m+n)//2 - mid
-            if mid > 0 and nums1[mid-1] > nums2[k]: hi = mid
-            elif mid < m and nums1[mid] < nums2[k-1]: lo = mid+1
-            else:
-                if mid == m: right = nums2[k]
-                elif k == n: right = nums1[mid]
-                else: right = min(nums1[mid], nums2[k])
-
-                if (m+n)%2: return right
-
-                if mid == 0: left = nums2[k-1]
-                elif k == 0: left = nums1[mid-1]
-                else: left = max(nums1[mid-1], nums2[k-1])
-
-                return (left + right)/2
-
-
-'''
-[1,2,4,6,7,8]
-     |
-     m
-
-mid =  5 + 0 // 2 = 2 --> pointing to 4, mid is also the count of numbers before mid
-remaining count of leftSide must come form the secont array.
-so if
-nums2 = [3,4,5]
-leftSide = 4 numbers
-numbers from nums2 = leftSide - m = 4 - 2, then index of mid2 is '2'
-
-then we should check cross check of sliced arrays
-
-
-'''
-
+        A ,B = (nums1, nums2) if len(nums1) < len(nums2) else (nums2, nums1)
+        total_len = (len(A)+len(B))
         
+        ### NOT Index, lenght of the portion
+        lo, hi = 0, len(A)
+        while lo <= hi:
+            # length of portions from both arrays
+            mid = lo + (hi - lo)//2
+            k = (total_len+1)//2 - mid
+
+            # edge values of sliced arrays
+            maxLeftA = float("-inf") if mid == 0 else A[mid-1]
+            minRightA = float("inf") if mid == len(A) else A[mid]
+            maxLeftB = float("-inf") if k == 0 else B[k-1]
+            minRightB = float("inf") if k == len(B) else B[k]
+
+            if maxLeftA <= minRightB and maxLeftB <= minRightA:
+                if total_len % 2 == 0:
+                    return (max(maxLeftA, maxLeftB) + min(minRightA, minRightB)) / 2
+                else:
+                    return max(maxLeftA, maxLeftB)
+
+            elif maxLeftA > minRightB:
+                hi = mid -1
+            else:
+                lo = mid +1
+
+
+
+
+
+
+            
+            
+
+
