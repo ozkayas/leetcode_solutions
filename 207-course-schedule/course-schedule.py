c9353a2e576faae3515c9f6f72fc604c64f62d14
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-26 21:30:42 +0300

    Time: 3 ms (98.73%) | Memory: 17.7 MB (98.74%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,43 +1,28 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # Kahns Algo:
-        orders = []
-        # Adjacency list, holds {pre:[course1,course2]} 
         adj = defaultdict(list)
-        in_degrees = [0 for _ in range(numCourses)]
+        inDegrees = [0 for _ in range(numCourses)]
         
-        ## Fill indegrees list, and adj list
-        for course, preReq in prerequisites:
-            in_degrees[course] += 1
-            adj[preReq].append(course)
-
-        ## Init BFS queue
-        bfs_queue = deque()
+        for to, fr in prerequisites:
+            adj[fr].append(to)
+            inDegrees[to] += 1
+        
+        bfsQ = deque()
         # find nodes without pre to start with
-        for i, inDegree in enumerate(in_degrees):
+        for i, inDegree in enumerate(inDegrees):
             if inDegree == 0:
-                bfs_queue.append(i)
-        
-        while bfs_queue:
-            cur = bfs_queue.popleft()
-
-            # put this in orders list and remove if this is a pre for any node indegrees list, 
-            orders.append(i)
-
-            # check if there is a cycle
-            # If orders > numCourses, this means there is a cycle
-            if len(orders) > numCourses:
-                return False
+                bfsQ.append(i)
+    
+        courses_order = []
+        while bfsQ:
+            cur = bfsQ.popleft()
+            courses_order.append(cur)
+             
+            if len(courses_order) > numCourses: return False
             
-            # decrese in_degrees for courses from this cur
-            for course in adj[cur]:
-                in_degrees[course] -= 1
-                if in_degrees[course] == 0:
-                    bfs_queue.append(course)
-                
-
-
-        return len(orders) == numCourses
-
-        
-        
+            #update inDegrees
+            for nei in adj[cur]:
+                inDegrees[nei] -= 1
+                if inDegrees[nei] == 0:
+                    bfsQ.append(nei)
+        return len(courses_order) == numCourses
