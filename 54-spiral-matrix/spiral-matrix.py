171f8d3de7848ee95cc37dc08a776f22238dc734
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-26 20:28:20 +0300

    Time: 0 ms (100.00%) | Memory: 16.9 MB (21.90%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.py
@@ -0,0 +1,40 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        # directions[0] gives the current direction
+        directions = deque([(0,1),(1,0),(0,-1),(-1,0)])
+
+        R, C = len(matrix), len(matrix[0])
+
+        def isValid(r, c) -> bool:
+            return 0 <= r < R and 0 <= c < C and matrix[r][c] != None
+
+        def nextCell(r, c) -> tuple:
+            dr, dc = directions[0]
+            if isValid(r+dr, c+dc):
+                return (r+dr, c+dc)
+            else: # change direction
+                directions.append(directions.popleft())
+                dr, dc = directions[0]
+                if isValid(r+dr,c+dc):
+                    return (r+dr, c+dc)
+                else:
+                    return None
+
+
+        ans = []
+        def explore(r, c):
+            ans.append(matrix[r][c])
+            matrix[r][c] = None # mark as visited by setting to none
+
+            nextRC = nextCell(r,c)
+            if not nextRC:
+                return
+            else:
+                rr, cc = nextRC
+                explore(rr,cc)
+
+
+        explore(0, 0)
+        return ans
+
+        
