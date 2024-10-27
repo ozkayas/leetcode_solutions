44bad9844330345590cf217224385b26cd82c46f
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-27 20:59:49 +0300

    Time: 0 ms (100.00%) | Memory: 16.6 MB (15.72%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,22 +1,14 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) < 3: return max(nums)
-        dp = [0 for _ in range(len(nums))]
-        dp[0]= nums[0]
-        dp[1]= nums[1]
-        
-        for i in range(2, len(nums)):
-            prev = dp[i-1]-nums[i-1]
-            prevprev = dp[i-2]
-            dp[i] = max(prev,prevprev) + nums[i]
-        
-        return max(dp[-1],dp[-2])
+        N = len(nums)
+        if N < 3: return max(nums)
+        dp = [0 for _ in range(N)]
+        dp[0], dp[1] = nums[0], nums[1]
 
 
-"""
-[2,7, 9, 1, 3,  1] 
- 2 7  11  
+        for i in range(2, N):
+            pre = dp[i-1] - nums[i-1]
+            prePre = dp[i-2]
+            dp[i] = max(pre, prePre) +nums[i]
 
- 2 1 1 2
- 2 1 
- """
+        return max(dp)
