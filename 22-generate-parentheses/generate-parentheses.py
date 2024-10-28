7012b39c1591d79a1244431d87fb95ebb01da286
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-28 22:23:37 +0300

    Time: 0 ms (100.00%) | Memory: 16.9 MB (57.14%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,21 +1,19 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        output = []
-
-        def generate(o:int, c:int, subString):
+        ans = []
+        def generate(o:int, c:int, current:str):
             if o == 0 and c == 0:
-                output.append(subString)
+                ans.append(current)
                 return
-
+            
             if o == c:
-                generate(o-1,c, subString +"(")
+                generate(o-1,c,current+"(")
             else:
                 if o > 0:
-                    generate(o-1,c, subString +"(")
-                generate(o, c-1, subString +")")
+                    generate(o-1,c,current+"(")
+                generate(o,c-1,current+")")
 
-
+        
         generate(n, n, "")
 
-        return output
-        
+        return ans 
