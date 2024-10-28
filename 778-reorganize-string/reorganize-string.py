149926e993e5aca258cd78c12ae5486e9e2ca944
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-28 16:37:16 +0300

    Time: 3 ms (52.89%) | Memory: 16.6 MB (72.74%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -1,23 +1,44 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        freq = list(Counter(s).items())
-        freq.sort(key = lambda i:i[1], reverse = True)
-        # print(freq)
+        N = len(s)
+        freq = defaultdict(int)
+        most = 0
+        mostCh = None
+        for c in s:
+            freq[c] += 1
+            if freq[c] >= most:
+                most = freq[c]
+                mostCh = c
 
-        #fast exit if most common can not be filled in the space
-        most = freq[0][1]
+        # aaab - NOK 4//2:2
+        # aaabb - OK 5//2:2  , aaaab - NOK
+        # aaaabb - NOK 6//2:3
         if most * 2 - 1 > len(s): return ""
 
-        ans = ["" for _ in range(len(s))]
-        i = 0 # global pointer first fill 0 2 4 6 ,then reset to 1 an fill 1 3 5 7 etc.
-        for ch, f in freq:
+        ### Sort ederek daha kisa bir kod yazilabilir, freq sort edilerek
+
+        output = ["" for _ in range(N)]
+        i = 0
+        # Fill most common char firstly
+        for _ in range(most):
+            output[i] = mostCh
+            i = i+2 if i+2<N else 1
+        # remove mostCh from table
+        del freq[mostCh]
+        # Insert remainings
+        for ch, f in freq.items():
             for _ in range(f):
-                # print(i, ch)
-                ans[i] = ch
-                i = i+2 if i+2 < len(s) else 1
-        
-        return "".join(ans)
+                output[i] = ch
+                i = i+2 if i+2<N else 1
+
+        return "".join(output)
+
+
 
 
         
+
+
+
+
         
