a5d6bed35d88ce14e504b2e8a717ea00954e1900
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-28 12:03:32 +0300

    Time: 0 ms (100.00%) | Memory: 16.5 MB (63.33%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,29 +1,17 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits: return ""
-
         cMap = {"2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
-        # print( list(cMap[digits[0]]) )
-        
-        def rec(digit)-> List[str]:
+
+        ans = []
+        def dfs(num:int, subSet:str):
+            if num == len(digits):
+                if subSet:
+                    ans.append(subSet)
+                return
             
-            if digit == len(digits)-1:
-                return list(cMap[digits[digit]])
-            
-            res = []
-            nxtNodeRes = rec(digit+1)
-            for c in cMap[digits[digit]]:
-                for cc in nxtNodeRes:
-                    # print(c+cc)
-                    res.append(c+cc)
-                    # print(res)
-            
-            return res
+            for ch in cMap[digits[num]]:
+                dfs(num+1, subSet+ch)
 
 
-        return rec(0)
-
-
-
-
-        
+        dfs(0, "")
+        return ans
