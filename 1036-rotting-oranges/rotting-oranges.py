5ac8bf21b3f21ae77cc66217b46c6b0b81f0a504
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-28 23:53:38 +0300

    Time: 5 ms (38.88%) | Memory: 16.7 MB (10.33%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,28 +1,36 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        R, C, minutes = len(grid), len(grid[0]), 0
-        freshSet, q = set(), []
-    
+        R, C = len(grid), len(grid[0])
+        fresh = 0
+        bfs = deque()
+        for r in range(R):
+            for c in range(C):
+                if grid[r][c] == 2:
+                    bfs.append((r,c)) 
+                elif grid[r][c] == 1:
+                    fresh +=1
+
+        if not bfs: 
+            return 0 if not fresh else -1
+
+        def isValidAndFresh(r,c):
+            return 0 <= r < R and 0 <= c < C and grid[r][c] == 1
+        
+        level = 0
+        while bfs:
+            for _ in range(len(bfs)):
+                r, c = bfs.popleft()
+                for rr,cc in [(r+1,c),(r-1,c),(r,c+1),(r,c-1)]:
+                    if isValidAndFresh(rr,cc):
+                        grid[rr][cc] = 2
+                        bfs.append((rr,cc))
+
+            level += 1
+
+        # Any remaining fresh after BFS
         for r in range(R):
             for c in range(C):
                 if grid[r][c] == 1:
-                    freshSet.add((r,c))
-                elif grid[r][c] == 2:
-                    q.append((r,c,0))
+                    return -1
         
-
-        while q:
-            r,c,minute = q.pop(0)
-
-            for rr,cc in (r-1,c),(r+1,c),(r,c-1),(r,c+1):
-                if 0<=rr<R and 0<=cc<C and grid[rr][cc] == 1:
-                    grid[rr][cc] = 2
-                    freshSet.remove((rr,cc,))
-                    q.append((rr,cc,minute+1))
-                    minutes = minute + 1
-
-
-        if len(freshSet) > 0:
-            return -1
-        
-        return minutes
+        return level-1
