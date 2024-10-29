7daa7c3ed5779021126ba1ee1413cf1ba5f27173
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-29 08:11:59 +0300

    Time: 2 ms (58.02%) | Memory: 16.8 MB (93.68%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,38 +1,27 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        A ,B = (nums1, nums2) if len(nums1) < len(nums2) else (nums2, nums1)
-        total_len = (len(A)+len(B))
-        
-        ### NOT Index, lenght of the portion
-        lo, hi = 0, len(A)
+        N1, N2 = len(nums1), len(nums2)
+        # Assum nums1 shorter
+        if N1 > N2:
+            return self.findMedianSortedArrays(nums2, nums1)
+        tot_len = N1 + N2
+
+        lo, hi = 0, N1 # Not index, lengths
         while lo <= hi:
-            # length of portions from both arrays
-            mid = lo + (hi - lo)//2
-            k = (total_len+1)//2 - mid
+            l1 = lo + (hi-lo)//2
+            l2 = (tot_len + 1)//2 - l1
+        
+            maxLeft1 = float("-inf") if l1 == 0 else nums1[l1-1]
+            maxLeft2 = float("-inf") if l2 == 0 else nums2[l2-1]
+            minRight1 = float("inf") if l1 == N1 else nums1[l1]
+            minRight2 = float("inf") if l2 == N2 else nums2[l2]
 
-            # edge values of sliced arrays
-            maxLeftA = float("-inf") if mid == 0 else A[mid-1]
-            minRightA = float("inf") if mid == len(A) else A[mid]
-            maxLeftB = float("-inf") if k == 0 else B[k-1]
-            minRightB = float("inf") if k == len(B) else B[k]
-
-            if maxLeftA <= minRightB and maxLeftB <= minRightA:
-                if total_len % 2 == 0:
-                    return (max(maxLeftA, maxLeftB) + min(minRightA, minRightB)) / 2
+            if maxLeft1 <= minRight2 and maxLeft2 <= minRight1:
+                if tot_len % 2 == 0:
+                    return (max(maxLeft1, maxLeft2) + min(minRight1, minRight2)) / 2
                 else:
-                    return max(maxLeftA, maxLeftB)
-
-            elif maxLeftA > minRightB:
-                hi = mid -1
+                    return max(maxLeft1, maxLeft2)
+            elif maxLeft1 > minRight2:
+                hi = l1-1
             else:
-                lo = mid +1
-
-
-
-
-
-
-            
-            
-
-
+                lo = l1 + 1
