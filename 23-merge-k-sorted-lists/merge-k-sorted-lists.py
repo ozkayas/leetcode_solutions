d48df188f836f2c8cdda263e33cf735d12d4be4d
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-10-30 10:22:34 +0300

    Time: 18 ms (27.53%) | Memory: 19.6 MB (45.69%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -4,28 +4,26 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        # need id to stop comparison between tuples and not compare node objects
         Node = namedtuple("Node", ["val","id","node"])
-        # fill heap with first nodes of each list
         minHeap = []
-        for ls in lists:
-            if ls:
-                minHeap.append(Node(ls.val, id(ls), ls))
+        for root in lists:
+            if root:
+                minHeap.append(Node(root.val, id(root), root))
         heapq.heapify(minHeap)
 
-        newHead = ListNode()
-        dummy = newHead
+        dummy = ListNode()
+        cur = dummy
+
         while minHeap:
-            _, __, curNode = heapq.heappop(minHeap)
-            dummy.next = curNode
-            dummy = dummy.next 
-            if curNode.next:
-                heapq.heappush(minHeap, Node(curNode.next.val, id(curNode.next),curNode.next))
-
-        return newHead.next
+            popped = heapq.heappop(minHeap)
+            cur.next = popped.node
+            cur = cur.next
+            toPutInHeap = popped.node.next
+            if toPutInHeap:
+                heapq.heappush(minHeap, Node(toPutInHeap.val, id(toPutInHeap), toPutInHeap))
         
-
+        return dummy.next
+        
 
         
