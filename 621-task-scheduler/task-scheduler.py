a5faabaa0fe9750ce605ccdb6bc60ba13e7e8689
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-11-01 13:55:03 +0300

    Time: 127 ms (51.96%) | Memory: 19 MB (5.12%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -1,19 +1,34 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         freq = Counter(tasks)
-        sortedTasks = sorted(freq.items(), key = lambda x:x[1])
+        maxHeap = [(-f, ch) for ch,f in freq.items()]
+        heapq.heapify(maxHeap)
 
-    #    print(sortedTasks) 
-        char, maxFreq = sortedTasks[-1]
+        output = []
+        # fill output while still tasks in the heap. Process cycle by cycle
+        while maxHeap:
+            cycle = n+1
+            tempNodes = [] # will push these into heap with updates frequencies
+            for _ in range(cycle):
+                if maxHeap:
+                    f, ch = heapq.heappop(maxHeap)
+                    output.append(ch)
+                    # reduce freq and save in tempNodes if still exists
+                    if f+1 < 0:
+                        tempNodes.append((f+1, ch))
+                elif tempNodes: # tempnode is not empty but heap is empty meand we need to add idle chars
+                    output.append("#")
+                elif not tempNodes and not maxHeap: # nothing left, just break
+                    break
 
-        #how many champions :)
-        champs = 0
-        for c,f in sortedTasks:
-            if f == maxFreq:
-                champs += 1
+            # After a cycle is filled, put tmpNodes back into the heap for next cycle
+            for node in tempNodes:
+                heapq.heappush(maxHeap, node)
+
+        return len(output)
 
 
-        # A . . A . . A => (freq-1)*n + freq
-        maxSizeForMostFreq = (maxFreq -1)*(n+1) + champs
 
-        return max(maxSizeForMostFreq, len(tasks))
+
+
+        
