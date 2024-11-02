8ff6783043d0cf059a9f60b9e3bb6a096132ba0c
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-11-02 17:57:11 +0300

    Time: 43 ms (14.53%) | Memory: 23.3 MB (89.67%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
@@ -1,13 +1,26 @@
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
-        visited = set()
+        # Cycle Sort: put 3 into index:2 , n -> arr[n-1]
+        # Then count the nums not in their expected place
+
         ans = []
 
-
-        for n in nums:
-            if n not in visited:
-                visited.add(n)
+        i = 0
+        while i < len(nums):
+            mustBeIndex = nums[i]-1
+            # right number in the place? We are at index i. so the number here should be i+1, if not swap or move i
+            if nums[mustBeIndex] != nums[i]: # Num to swaps should be different, if they are same -> infinite loop
+                nums[i], nums[mustBeIndex] = nums[mustBeIndex], nums[i]
             else:
+                i += 1
+
+        # now the array must be sorted [1,2,3,,,.]
+        for i,n in enumerate(nums):
+            if i+1 != n:
                 ans.append(n)
 
         return ans
+
+
+
+        
