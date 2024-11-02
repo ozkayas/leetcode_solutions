dfad0202d5168b72310811cb9d9c04d51ba8affa
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-11-02 15:48:25 +0300

    Time: 4 ms (27.82%) | Memory: 16.5 MB (58.44%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -1,12 +1,14 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
+        # Must put reversely. 9 before 5, 5 before 4,3,2... 
+        # 34 before 3, 3 before 30
         def compare(a:int, b:int) -> int:
+            # standard compare function -1 if a<b
             if a == b: return 0
             return -1 if str(a)+str(b) < str(b)+str(a) else 1
 
-        
-        nums.sort(key = cmp_to_key(compare),reverse = True)
-        # print(nums)
+        nums.sort(key = cmp_to_key(compare), reverse = True)
+
         if nums[0] == 0: return "0"
 
         return "".join([str(n) for n in nums])
