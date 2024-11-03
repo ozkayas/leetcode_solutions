e7866f3e3c6144bd9108ba53443365a24236a94b
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-11-04 00:50:13 +0300

    Time: 19 ms (25.12%) | Memory: 17.5 MB (9.24%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -1,28 +1,41 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        n = numRows
-        if n == 1: return s
-        arr = [[] for _ in range(n)]
-        diff = -1
-        curRow = 0
-        res = ""
-        
-        def changeDir():
-            nonlocal diff
-            diff = -1 * diff
-            # print("changed dir to: ", diff)
+        if numRows == 1: return s
+        buckets = [deque() for _ in range(numRows)]
+        i = 0
+        forward = True # to reverse the direction of writing
 
-        for i in range(len(s)):
-            arr[curRow].append(s[i])           
+        def updateI():
+            nonlocal i, forward
+            if forward:
+                if i == numRows - 1:
+                    forward = False
+                    i -= 1
+                else: 
+                    i += 1
+            else:
+                if i == 0:
+                    forward = True
+                    i += 1
+                else: 
+                    i -= 1
+        for ch in s:
+            # process
+            buckets[i].append(ch)
+            updateI()
 
-            if i % (n-1) == 0:
-                changeDir()
-            curRow += diff
-        
-        for ar in arr:
-            res += "".join(ar)
+        output = []
+        for bucket in buckets:
+            output.extend(bucket)
 
-        # print(arr)
-        return res
+        return "".join(output)
 
+"""
+"PAYPALISHIRING"
+4 deques:
+0:PIN
+1:ALsiG
+2:YAHR
+3:PI
 
+"""
