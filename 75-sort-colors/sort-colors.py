7eb70e876d0a94e2c2494130dcd18a79211e515d
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-11-04 16:25:25 +0300

    Time: 0 ms (100.00%) | Memory: 16.6 MB (51.60%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,23 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        l, r, i = 0, len(nums)-1, 0
+
+        l, i, r = 0, 0, len(nums)-1
 
         while i <= r:
-            if nums[i] == 2:
-                nums[i], nums[r] = nums[r], nums[i]
-                r -= 1
+            if nums[i] == 1:
+                i += 1
             elif nums[i] == 0:
                 nums[i], nums[l] = nums[l], nums[i]
+                i += 1
                 l += 1
-                i += 1
             else:
-                i += 1
+                nums[i], nums[r] = nums[r], nums[i]
+                r -= 1
         
-
-
-    
-
-
-
-  
+        
+        
