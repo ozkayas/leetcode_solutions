871b80cc08bc0b1e8be47c4b199dfdb619a69d1c
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-11-10 12:05:48 +0300

    Time: 32 ms (78.87%) | Memory: 20.4 MB (5.25%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,59 +1,35 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        open_p, close_p = 0, 0
-        ans_list = []
+        opens, closes = 0, 0
+        stack = []
 
-        ## 2 tur atmamiz gerekiyor, ilkinde ( kullanmak serbest ama ) sayisini kontrol altinda tutuyoruz
-        ## 2. turu tersten atiiyoruz, bu kez fazla ( varsa bunlari siliyoruz.
-
-        for c in s:
-            if c == "(":
-                open_p += 1
-                ans_list.append(c)
-            elif c == ")":
-                if open_p > close_p:
-
-                    close_p += 1
-                    ans_list.append(c)
+        # First pass: add all "(", do not add ")" if has no match.
+        for ch in s:
+            if ch == "(":
+                opens += 1
+                stack.append(ch)
+            elif ch == ")":
+                if opens > closes: # We can add closing
+                    closes += 1
+                    stack.append(ch)
             else:
-                ans_list.append(c)
-                
-
-        ## acma kapamalar esit sonucu bulduk
-        if open_p == close_p:
-            return "".join(ans_list)
-
-        ## ac parantezler daha fazla onlari tersten giderek iptal etmeliyiz.
-        else:
-            res = []
-
-            for i in range(len(ans_list)-1,-1,-1):
-                cur = ans_list[i]
-
-                if cur == "(":
-                    if open_p <= close_p:
-                        res.append(cur)
-                    else:
-                        open_p -= 1
-                elif cur == ")":
-                    res.append(cur)
-                else:
-                    res.append(cur)
-
-            return "".join(reversed(res))
+                stack.append(ch)
         
+        if opens == closes:
+            return "".join(stack)
+
+        # Now strBuilder may have excessive openings.
+        # We reverse iterate with the current states of opens & closes and process
+        strBuilder = []
+        for ch in reversed(stack):
+            if ch == "(" and opens > closes:
+                opens -= 1
+                continue
+            else:
+                strBuilder.append(ch)
+
+        return "".join(reversed(strBuilder))
 
 
 
-
-
-
-
-
-
-
-'''if open just add to stack
-if close check last, if open remove both and continue, if empty or last is close, do not add this paranthesis
-stack: (()
-
-lee ( t ( c ) o ) de )'''
+        
