6cdd73502cb091a872022b7bdf4cf292074c8d7f
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-11-14 22:23:24 +0300

    Time: 0 ms (100.00%) | Memory: 16.9 MB (15.02%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,25 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+
         ans = []
-
-        def backTrack(i, subset):
-            # print("BT called i, subset",i, subset)
-            #bu son index ise, leaf node, subsete kendin ekle ve sonuca ekle ve return
-            if i == len(nums)-1:
-                s = subset.copy()
-                ans.append(s.copy())
-                s.append(nums[i])
-                ans.append(s.copy())
-                # print("leaf node returning", ans)
+        def dfs(i:int, subSet):
+            if i == len(nums):
+                ans.append(subSet.copy())
                 return
+            dfs(i+1, subSet)
+            subSet.append(nums[i])
+            dfs(i+1, subSet)
+            subSet.pop()
 
-            subset.append(nums[i])
-            backTrack(i+1, subset)
-            subset.pop()
-            backTrack(i+1, subset)
-
-        
-        backTrack(0,[])
+        dfs(0, [])
 
         return ans
         
