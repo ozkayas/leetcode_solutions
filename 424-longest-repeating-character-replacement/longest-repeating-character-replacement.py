968d140c66d768715821dd640ce9eadb953931ee
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-11-22 12:06:34 +0300

    Time: 143 ms (12.69%) | Memory: 17.1 MB (25.72%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,28 +1,27 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        l = 0
         freq = defaultdict(int)
-        max_len = 0
-        most_freq_char = 0
+        maxCharCount = 0
+        maxWindow = 0
 
-        for r in range(len(s)):
-            # At each iteration recalculate window len, and freq of chars
-            w_len = (r-l+1)
+        l = r = 0
+        def changableChars() -> int:
+            windowLen = r - l + 1
+            return windowLen - maxCharCount
+
+        while r < len(s):
             freq[s[r]] += 1
-            most_freq_char = max(most_freq_char, freq[s[r]])
-            swappable_chars_count = w_len - most_freq_char
-            # print(freq.items())
-
-            if swappable_chars_count > k: # Not a valid substring, can not form a AAAAA, so shrink window
+            maxCharCount = max(maxCharCount, freq[s[r]])
+            # expand window while changableChars <= k
+            # else shrink it
+            while changableChars() > k:
                 freq[s[l]] -= 1
                 l += 1
-                w_len -= 1
 
-            max_len = max(max_len, w_len)
+            maxWindow = max(maxWindow, (r-l+1))
+            r += 1
+        
+        return maxWindow
 
-        # print(l,r,freq.items(),max_len)
-        return max_len
-
-
-
+        
         
