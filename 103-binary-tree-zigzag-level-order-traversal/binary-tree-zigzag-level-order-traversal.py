baee675ecd34fae3af25d877cb1dba27bca9e7de
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2024-11-26 13:30:19 +0300

    Time: 3 ms (8.16%) | Memory: 17.3 MB (20.65%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
@@ -6,35 +6,26 @@
 #         self.right = right
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root: return []
-        bfsStack = deque([(root, 0)]) 
-        # Fill the temp when push a node to bfsStack
-        ans, temp = [], []
-
-        def fillAns():
-            isLevelEven = temp[-1][1] %2 == 0 
-            if isLevelEven:
-                ans.append([n.val for n,l in temp])
-            else:
-                ans.append([n.val for n,l in reversed(temp)])
-
-
-        while bfsStack:
-            cur, level = bfsStack.popleft()
-            # add temp to ans if last level is different than this level
-            if temp and temp[-1][1] != level:
-                fillAns()
-                temp.clear()
-            
-            temp.append((cur, level))
-            if cur.left:
-                bfsStack.append((cur.left, level+1))
-            if cur.right:
-                bfsStack.append((cur.right, level+1))
+        if not root:
+            return []
+        output = []
+        bfsQ = deque([root])
+        direction = 1
+        while bfsQ:
+            temp = []
+            for _ in range(len(bfsQ)):
+                cur = bfsQ.popleft()
+                temp.append(cur.val)
+                if cur.left:
+                    bfsQ.append(cur.left)
+                if cur.right:
+                    bfsQ.append(cur.right)
                 
-        fillAns()
-        
-        return ans
+            if direction == 1:
+                output.append(temp)
+            else:
+                output.append(temp[::-1])
+            
+            direction *= -1
 
-
-        
+        return output
