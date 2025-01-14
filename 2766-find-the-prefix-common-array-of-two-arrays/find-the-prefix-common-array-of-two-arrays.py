e15addbfe7d1f5e323e057a283caa716908f0a43
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2025-01-14 16:23:41 +0300

    Time: 7 ms (75.00%) | Memory: 17.7 MB (32.03%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
@@ -1,13 +1,37 @@
 class Solution:
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
-        seta = set()
-        setb = set()
+        N = len(A)
+        # bool array to mark seen
+        arr_a = [False for _ in range(N+1)]
+        arr_b = [False for _ in range(N+1)]
         res = []
+        lastStepCommon = 0
 
-        for i in range(len(A)):
-            seta.add(A[i])
-            setb.add(B[i])
+        for i in range(N):
+            a, b = A[i], B[i]
+            arr_a[a] = True
+            arr_b[b] = True
+            if a == b:
+                lastStepCommon += 1
+            else:
+                if arr_a[a] and arr_b[a]:
+                    lastStepCommon +=1
+                if arr_a[b] and arr_b[b]:
+                    lastStepCommon +=1
 
-            res.append(len(seta.intersection(setb)))
+            res.append(lastStepCommon)
         
         return res
+
+
+        # seta = set()
+        # setb = set()
+        # res = []
+
+        # for i in range(len(A)):
+        #     seta.add(A[i])
+        #     setb.add(B[i])
+
+        #     res.append(len(seta.intersection(setb)))
+        
+        # return res
