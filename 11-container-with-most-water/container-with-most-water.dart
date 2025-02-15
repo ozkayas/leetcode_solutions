d978263e6ccae0bd8f0ce518c1f3d3792d603d70
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2025-02-15 15:49:36 +0300

    Time: 1 ms (100.00%) | Memory: 171.4 MB (71.03%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.dart
@@ -0,0 +1,22 @@
+class Solution {
+  int maxArea(List<int> height) {
+
+    int l = 0;
+    int r = height.length-1;
+
+    int water =0;
+    int curWater = 0;
+
+    while(l < r){
+        curWater = (r-l) * min(height[l], height[r]);
+        water = max(water, curWater);
+
+        if (height[l] < height[r]){
+            l++;
+        }else{
+            r--;
+        }
+    }
+    return water;
+  }
+}
