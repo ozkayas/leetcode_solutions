271b23df815a3fb4ab1c3292b331604939369217
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2025-05-07 15:19:37 +0300

    Time: 10 ms (18.05%) | Memory: 17.6 MB (90.28%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,26 +1,28 @@
-from typing import List
-
 class Solution:
     def romanToInt(self, s: str) -> int:
-        m = {"I":1, "V":5, "X": 10, "L":50, "C":100, "D":500, "M":1000}
+        table = {"I":1,
+                    "V":5,
+                    "X":10,
+                    "L":50,
+                    "C":100,
+                    "D":500,
+                    "M":1000}
 
-        i = val = 0
-        while i < len(s)-1:
-            this = m[s[i]]
-            nxt = m[s[i+1]]
-             
-            if this >= nxt:
-                val += this
-                i += 1
-            else: # IV 5-1
-                val += (nxt - this)
-                i += 2
+        lastAdded = 0
+        total = 0
+        for roman in s:
+            if lastAdded == 0:
+                total += table[roman]
+                lastAdded = table[roman]
+            else:
+                if table[roman] <= lastAdded:
+                    total += table[roman]
+                    lastAdded = table[roman]
+                else:
+                    total -= lastAdded*2
+                    total += table[roman]
+                    lastAdded = table[roman]
+        return total
 
-        if i == len(s)-1:
-            val += m[s[i]]
 
-        return val
-            
         
-
-
