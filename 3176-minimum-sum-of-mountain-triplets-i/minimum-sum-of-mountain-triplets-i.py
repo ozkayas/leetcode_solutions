28207ffb548950d210f11b37a1264414202dddcb
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2025-05-07 12:10:11 +0300

    Time: 50 ms (71.61%) | Memory: 17.6 MB (76.71%) - LeetSync

--- 3176-minimum-sum-of-mountain-triplets-i/minimum-sum-of-mountain-triplets-i.py
+++ 3176-minimum-sum-of-mountain-triplets-i/minimum-sum-of-mountain-triplets-i.py
@@ -1,33 +1,38 @@
 class Solution:
     def minimumSum(self, nums: List[int]) -> int:
-        N = len(nums)
-        # use monotonic stack from both sides to keep track the min at left for each index
-        # [8, 6, 1, 1, 1] and [1, 1, 3, 3, 3]
-        leftMin = deque([nums[0]])
-        rightMin = deque([nums[-1]])
-        minn = nums[0]
-        for i in range(1, N):
-            if nums[i] < minn:
-                minn = nums[i] 
-            leftMin.append(minn)
-        
-        minn = nums[-1]
-        for i in range(N-1, 0, -1):
-            if nums[i] < minn:
-                minn = nums[i] 
-            rightMin.appendleft(minn)
-        # print(f"leftMin: {leftMin}")
-        # print(f"rightMin: {rightMin}")
+        '''        # for each top point, what is the min on left and min on right
+                # Then check for each top point if it is a mountain
+                # [5,4,8,7,10,2] - loop [1,n-1] range
+                #  5,4,4,4,4,2
+                #  2,2,2,2,2,2
+        '''
+        def getMonoStack(nums:List[int]) -> List:
+            minlist = []
+            curMin = nums[0]
+            for n in nums:
+                if n < curMin:
+                    curMin = n
+                minlist.append(curMin)
+            return minlist
 
-        ans = float('inf')
-        for i in range(1, N-1):
-            if leftMin[i] < nums[i] > rightMin[i]:
-                ans = min(ans,leftMin[i] + nums[i] + rightMin[i] )
+        monoLeft = getMonoStack(nums)
+        monoRight = list(reversed(getMonoStack(list(reversed(nums)))))
+        # print(monoLeft)
+        # print(monoRight)
+    
+        output = float('inf')
+        for i in range(1, len(nums)-1):
+            # This index is not the top of a mountain
+            if monoLeft[i] >= nums[i] or monoRight[i] >= nums[i]:
+                continue
+
+            curVal = monoLeft[i] + nums[i] + monoRight[i]
+            output = min(output, curVal)
+
+        return -1 if output == float('inf') else output
 
 
-        return -1 if ans == float('inf') else ans
 
-            
 
 
 
