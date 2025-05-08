041f8cb4f84be9fec2126869b6a0e8bd2c425e6f
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2025-05-08 16:20:59 +0300

    Time: 1534 ms (5.00%) | Memory: 85.9 MB (86.15%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -1,47 +1,35 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        visited = set()
-        adjList = [[] for _ in range(n)]
-        for e in edges:
-            u, v = e[0], e[1]
-            adjList[u].append(v)
-            adjList[v].append(u)
+        # UnionFind solution
+        parent = dict()
+        for i in range(n):
+            parent[i] = i #Assign each as its root
 
-        def dfs(v) -> bool:
-            visited.add(v)                
-            if v == destination:
-                    return True
+        def find(i:int) -> int:
+            root = i
 
-            for u in adjList[v]:
-                if u not in visited:
-                    if dfs(u):
-                        return True
-            return False
+            while root != parent[root]:
+                root = parent[root]
+            
+            # Compression, connect intermediate nodes to root directly as a parent
+            cur = i
+            while cur != root: # do this until reaching root.
+                curParent = parent[cur]
+                parent[curParent] = root
+                cur = curParent
+
+            return root
+
+        def union(u:int, v:int):
+            rootU = find(u)
+            rootV = find(v)
+            parent[rootV] = rootU
+
+        for u,v in edges:
+            union(u,v)
         
-        return dfs(source)
+        return find(source) == find(destination)
 
-        # visited = set()
-        # path = [[] for _ in range(n)]
+        
 
-        # for edge in edges:
-        #     path[edge[0]].append(edge[1])
-        #     path[edge[1]].append(edge[0])
-
-        # # print(path)
-        # def dfs(s) -> int:
-        #     # print("s:{}, dest:{}".format(s, destination))
-        #     visited.add(s)
-        #     if s == destination:
-        #         return True
-        #     for neigh in path[s]:
-        #         if neigh not in visited:
-        #             if dfs(neigh):
-        #                 return True
-            
-
-        #     return False
-            
-
-
-
-        # return dfs(source)   
+        
