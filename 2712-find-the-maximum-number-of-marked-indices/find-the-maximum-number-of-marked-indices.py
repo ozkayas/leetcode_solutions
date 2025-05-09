51ae92ab3fbf54b05d3a45e326c0ca9f191e9048
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2025-05-09 16:19:53 +0300

    Time: 64 ms (98.09%) | Memory: 31.5 MB (81.82%) - LeetSync

--- 2712-find-the-maximum-number-of-marked-indices/find-the-maximum-number-of-marked-indices.py
+++ 2712-find-the-maximum-number-of-marked-indices/find-the-maximum-number-of-marked-indices.py
@@ -1,19 +1,18 @@
 class Solution:
     def maxNumOfMarkedIndices(self, nums: List[int]) -> int:
-        N, counter = len(nums), 0
         nums.sort()
+        N = len(nums)
+        marked = 0
         l, r = 0, (N+1)//2
 
-        while r < N:
+        # Two pointer checking
+        while r < N :
             if nums[l]*2 <= nums[r]:
-                l += 1 
-                r += 1 
-                counter += 2
+                marked += 2
+                l += 1
+                r += 1
             else:
                 r += 1
-        
-        return counter
-            
-
+        return marked
 
         
