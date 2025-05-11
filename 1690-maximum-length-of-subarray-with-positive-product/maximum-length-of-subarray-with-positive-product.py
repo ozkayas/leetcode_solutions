f7be024497232dc4fe8fe789094172fd8a46bb97
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2025-05-11 12:46:10 +0300

    Time: 27 ms (98.24%) | Memory: 30.8 MB (24.34%) - LeetSync

--- 1690-maximum-length-of-subarray-with-positive-product/maximum-length-of-subarray-with-positive-product.py
+++ 1690-maximum-length-of-subarray-with-positive-product/maximum-length-of-subarray-with-positive-product.py
@@ -1,46 +1,39 @@
 class Solution:
-    def splitList(self,lst) -> List[int]:
-        result = []
-        current = []
-    
-        for item in lst:
-            if item == 0:
-                if current:  # current list boş değilse
-                    result.append(current)
-                    current = []
-            else:
-                current.append(item)
-    
-        if current:  # son grup boş değilse onu da ekle
-            result.append(current)
-    
-        return result
-
-    def maxLenOf(self, subarr: List[int]) -> int:
-        positives, N = 0, len(subarr)
-        for n in subarr:
-            if n < 0: positives += 1
-        
-        if positives % 2 == 0:
-            return len(subarr)
-        else:
-            firstLeftNegative, firstRightNegative = 0, N-1
-            while subarr[firstLeftNegative] > 0:
-                firstLeftNegative += 1
-            while subarr[firstRightNegative] > 0:
-                firstRightNegative -= 1
-            # print(firstLeftNegative, firstRightNegative)
-            return max(N - firstLeftNegative -1, firstRightNegative) 
-
-
     def getMaxLen(self, nums: List[int]) -> int:
-        maxLen = 0
-        subarrays = self.splitList(nums)
-        # print(subarrays)
+        def splitList(nums) -> List[List[int]]:
+            output, cur = [], []
+            for n in nums:
+                if n == 0:
+                    if cur:
+                        output.append(cur)
+                        cur = []
+                else:
+                    cur.append(n)
+            if cur:
+                output.append(cur)
+            return output
+        def maxLenOf(subarr: List[int]) -> int:
+            negatives, N = 0, len(subarr)
+            for n in subarr:
+                if n < 0: negatives += 1
+            
+            if negatives % 2 == 0:
+                return len(subarr)
+            else:
+                firstLeftNegative, firstRightNegative = 0, N-1
+                while subarr[firstLeftNegative] > 0:
+                    firstLeftNegative += 1
+                while subarr[firstRightNegative] > 0:
+                    firstRightNegative -= 1
+                # print(firstLeftNegative, firstRightNegative)
+                return max(N - firstLeftNegative -1, firstRightNegative) 
 
-        for subarr in subarrays:
-            maxLen = max(maxLen, self.maxLenOf(subarr))
-        return maxLen
+        splitted = splitList(nums)
+
+        maxSoFar = 0
+        for lst in splitted:
+            maxSoFar = max(maxSoFar, maxLenOf(lst))
 
 
+        return maxSoFar
         
