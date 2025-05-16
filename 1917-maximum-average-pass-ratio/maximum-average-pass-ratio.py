fc11518cd9752a18f0bdcd7e0227658bac4c9d83
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2025-05-16 20:25:46 +0300

    Time: 1096 ms (79.06%) | Memory: 54.9 MB (64.84%) - LeetSync

--- 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.py
+++ 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.py
@@ -1,24 +1,21 @@
 class Solution:
     def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
         maxHeap = []
-        for a, b in classes:
-            # possible upgrade gain of this class
-            upgrade = (a+1)/(b+1) - (a/b)
-            maxHeap.append((-upgrade, a, b))
 
-        heapq.heapify(maxHeap)
+        for a, b in classes:
+            possibleGain = (a+1)/(b+1) - a/b
+            heapq.heappush(maxHeap, (-possibleGain, a, b))
 
         while extraStudents:
-            # pop the class with max possible gain and add +1 student to it
             _, a, b = heapq.heappop(maxHeap)
-            a+=1
-            b+=1
-            upgrade = (a+1)/(b+1) - a/b
-            heapq.heappush(maxHeap,(-upgrade, a, b))
+            a += 1
+            b += 1
+            possibleGain = (a+1)/(b+1) - a/b
+            heapq.heappush(maxHeap, (-possibleGain, a, b))
 
             extraStudents -= 1
-        
-        total = sum([a/b for _,a,b in maxHeap])
-        
-        return total/len(maxHeap)
+
+        total = sum([a/b for _ ,a ,b in maxHeap])
+        return total/len(classes)
+
         
