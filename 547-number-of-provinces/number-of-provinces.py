c2a10ad72f7076698e3d5fbf1e6e01ab5d44450e
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2025-05-19 10:00:04 +0300

    Time: 3 ms (89.55%) | Memory: 19.5 MB (16.18%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,43 +1,23 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         N = len(isConnected)
-        parent = [n for n in range(N+1)]
+        visited = set()
+        provinces = 0
 
-        # finds the parent/root a node
-        def find(n: int) -> int:
-            root = n
-            while root != parent[root]:
-                root = parent[root]
+        def dfs(i:int):
+            visited.add(i)
+            # loop on neighs
+            for j in range(N):
+                if i != j and isConnected[i][j] and (j not in visited):
+                    dfs(j)
+                    
 
-            while n != root:
-                next_node = parent[n] 
-                parent[n] = root
-                n = next_node
         
-            return root 
-
-        # bind: parent of v --> u
-        def union(u: int, v: int):
-            # print(f"union.. {u}, {v}")
-            # print(parent)
-            rootOfU = find(u)
-            rootOfV = find(v)
-            parent[rootOfV] = rootOfU
-            # print(parent)
-        
-        # we will union all isConnecteds
         for i in range(N):
-            for j in range(i+1, N):
-                if isConnected[i][j]:
-                    union(i+1, j+1)
+            if i not in visited:
+                provinces += 1
+                dfs(i)
 
-        # print(parent)
+        return provinces
 
-        for i in range(1, N+1):
-            find(i)  # Ensures path compression is applied to all nodes
-
-
-
-        # return the set of parents list
-        return len(set(parent[1:]))
         
