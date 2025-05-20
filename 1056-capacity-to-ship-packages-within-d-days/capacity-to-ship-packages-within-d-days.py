cc23cbbb767a4393dfac4611853a175ef98538b4
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2025-05-20 16:56:24 +0300

    Time: 182 ms (70.31%) | Memory: 22.1 MB (55.49%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -1,41 +1,30 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
 
-        # helper method, calculate the required days for a given ship capacity
-        def countDays(capacity: int)-> int:
+        def daysForCapacity(c:int) -> int: # Gives the days required for the given capacity
+            total = 0
             days = 0
-            temp = 0
             for w in weights:
-                if w + temp > capacity:
+                if (total + w) > c:
                     days += 1
-                    temp = w
+                    total = w
                 else:
-                    temp += w
+                    total += w
+            return days+1
 
-            return days + 1
-            
 
-        
-        # Binary search between 10 - 55 // first Example
-        # Can extract as a helper method, for O(N)
         lo, hi = max(weights), sum(weights)
-
-        # Best ship capacity so far, we are trying to minimize it
         bestSoFar = hi
         while lo <= hi:
             mid = lo + (hi-lo)//2
 
-            if countDays(mid) <= days:
-                # This is good, this capacity [mid] is ok. lets save it and continue for a better one
-                bestSoFar = mid
-                hi = mid - 1
-            else:
+            if daysForCapacity(mid) > days: # not good we should have a bigger ship
                 lo = mid + 1
-
-
+            else:
+                bestSoFar = mid
+                hi = mid -1
+            
         return bestSoFar
-
-
 
 
         
