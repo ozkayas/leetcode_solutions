67fc13e99124183230ae4d55e140989ae2f1137c
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2025-05-30 13:56:51 +0300

    Time: 2 ms (18.16%) | Memory: 17.7 MB (69.21%) - LeetSync

--- 1304-longest-happy-string/longest-happy-string.py
+++ 1304-longest-happy-string/longest-happy-string.py
@@ -1,26 +1,26 @@
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
-        fHeap = []
-        for f, ch in zip((a,b,c), "abc"):
-            if f: heapq.heappush(fHeap, (-f, ch)) 
+        mxHeap = [] 
+        for f, ch in zip([a,b,c],"abc"):
+            if f: heapq.heappush(mxHeap, (-f, ch))
 
         ans = []
-        while fHeap:
-            f, c = heapq.heappop(fHeap)
-            if ans[-2:] == [c, c]:
-                if not fHeap: break
-                ff, cc = heapq.heappop(fHeap)
+        while mxHeap:
+            f, c = heapq.heappop(mxHeap)
+            if ans[-2:] == [c,c]:
+                if not mxHeap:
+                    break
+                # try the second most
+                ff, cc = heapq.heappop(mxHeap)
                 ans.append(cc)
-                # add remaining cc if exists still
                 if -ff-1 > 0:
-                    heapq.heappush(fHeap,(ff+1, cc))
-                heapq.heappush(fHeap, (f,c))
+                    heapq.heappush(mxHeap, (ff+1, cc))
+
+                # Put unused ch back in the heap
+                heapq.heappush(mxHeap, (f,c))
             else:
                 ans.append(c)
                 if -f-1 > 0:
-                    heapq.heappush(fHeap,(f+1, c))
+                    heapq.heappush(mxHeap, (f+1,c))
+
         return "".join(ans)
-                
-
-
-        
