55be6547b5c3932244d178c29e4dce6622a63525
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2025-06-14 21:47:40 +0300

    Time: 6 ms (88.31%) | Memory: 20.6 MB (59.88%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.py
+++ 1397-search-suggestions-system/search-suggestions-system.py
@@ -1,27 +1,15 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        ans = []
         products.sort()
+        output = []
 
         for i in range(len(searchWord)):
-            query = searchWord[0:i+1]
-            idx = bisect_left(products,query)
-            found =  []
-            for j in range(idx, idx+3):
-                if j < len(products) and query == products[j][0:i+1]:
-                    found.append(products[j])
-            ans.append(found)
-        # print(ans)
-        return ans
+            target = searchWord[:i+1]
+            idx = bisect_left(products, target)
+            subset = []
+            for j in range(idx,idx+3):
+                if j < len(products) and target == products[j][:i+1] :
+                    subset.append(products[j])
+            output.append(subset)
 
-
-
-"""
-"mobile",
-"moneypot"
-"monitor"
-"mousemonitor",
-"mousepad"
-
-"""
-        
+        return output
