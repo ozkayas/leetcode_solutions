0914bb93754d95771e8bfe4ddd0cbb749b533c42
Suat Özkaya <suatozkaya@gmail.com>
2025-06-23 22:08:16 +0300

    Add initial implementation of Cart and Item classes with promotion criteria

---
+++ LLD_OOD/design_cart_item.py
@@ -0,0 +1,12 @@
+'''
+You're working at Amazon retail and you're in-charge of implementing a method/function to be used by the Promotions team to apply a discount to each individual item in the checkout cart. The function is used to find items that meets a certain promotion criteria.
+
+There are currently two on-going promotions, the criteria for each promotion is as follows:
+
+Items in the cart that are a certain price (e.g. >$20).
+Items in the cart that belong to a certain product group. Product groups can be (Clothes, Hardware, Media, Toy, Food).
+Given this information:
+
+Design the Cart and Item classes
+Create the method/function that will find the items for each of the use cases above
+'''
