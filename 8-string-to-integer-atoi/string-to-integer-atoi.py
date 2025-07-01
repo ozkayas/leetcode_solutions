df72a3fd85b7178284c7cda17e40f8204f07ea52
Suat Özkaya <63163416+ozkayas@users.noreply.github.com>
2025-07-01 12:31:34 +0300

    Time: 2 ms (46.76%) | Memory: 18 MB (8.51%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,23 +1,25 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        N = len(s)
-        sign, number = 1, 0
+        if not s: return 0
+        cur, sign, N = 0, 1, len(s)
         i = 0
-        while i < N and s[i] == " ":
+
+        while i < N and s[i] == " ": # Skip empty
             i += 1
         if i == N:
-            return number
+            return cur
         if s[i] in  {"-","+"}:
             sign = -1 if s[i] == "-" else 1
             i +=1
-        
+
         while i < N and s[i] == "0":
             i += 1
-
+        
         res = 0
         while i < N and s[i].isdigit():
             res = res * 10 + int(s[i])
             i += 1
+        
 
         # 32-bit integer sınırlarını kontrol et (opsiyonel)
         INT_MAX = 2**31 - 1
@@ -32,3 +34,9 @@ class Solution:
                 return INT_MAX
 
         return res
+            
+            
+            
+
+
+        
