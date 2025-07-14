6ed7839822c5a9eef0082bcb8d2c098e30aa8e32
Suat Özkaya <suatozkaya@gmail.com>
2025-07-14 16:47:31 +0300

    Refactor: Remove unused scripts and classes related to algorithms and data structures

--- LLD_OOD/call_center/caller.py
+++
@@ -1,5 +0,0 @@
-class Caller:
-
-    def __init__(self, name: str, userId: int):
-        self.name = name
-        self.userId = userId
--- LLD_OOD/call_center/rank.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class Rank(Enum):
-     Responser = 1
-     Manager = 2
-     Director = 3
